66c7b89c12cf7164ef80d24889d9f92d5c42e43c
Martin Humphrey <martin.humphrey@ons.gov.uk>
2016-03-04 16:40:38 +0000

    CTPA-267 Transaction time out set on response operation

--- caseframesvc/src/main/java/uk/gov/ons/ctp/response/caseframe/service/QuestionnaireService.java
+++ caseframesvc/src/main/java/uk/gov/ons/ctp/response/caseframe/service/QuestionnaireService.java
@@ -2,19 +2,18 @@ package uk.gov.ons.ctp.response.caseframe.service;
 
 import java.util.List;
 
-import javax.ws.rs.core.Response;
-
 import uk.gov.ons.ctp.common.service.CTPService;
 import uk.gov.ons.ctp.response.caseframe.domain.model.Questionnaire;
 
 /**
- * The Questionnaire Service interface defines all business behaviours
- * for operations on the Questionnaire entity model.
+ * The Questionnaire Service interface defines all business behaviours for
+ * operations on the Questionnaire entity model.
  */
 public interface QuestionnaireService extends CTPService {
 
   /**
-   * Find Questionnaire entity by Internet Access Code
+   * Find Questionnaire entity by Internet Access Code.
+   * 
    * @param IAC
    * @return Questionnaire object or null
    */
@@ -22,6 +21,7 @@ public interface QuestionnaireService extends CTPService {
 
   /**
    * Find Questionnaire entities associated with a Case.
+   * 
    * @param Case Id
    * @return List of Questionnaire entities or empty List
    */
@@ -30,10 +30,10 @@ public interface QuestionnaireService extends CTPService {
   /**
    * Update a Questionnaire and Case object to record a response has been
    * received in the Survey Data Exchange.
+   * 
    * @param Questionnaire Id
    * @return Updated Questionnaire object
    */
   Questionnaire recordResponse(Integer questionnaireid);
-
 
 }
