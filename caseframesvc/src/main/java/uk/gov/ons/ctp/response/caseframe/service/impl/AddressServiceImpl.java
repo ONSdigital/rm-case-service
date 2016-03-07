8412fe2df955ed7deac0227f46d6120392b10b83
Martin Humphrey <martin.humphrey@ons.gov.uk>
2016-03-07 17:01:37 +0000

    Checkstyle fixes AddressServiceImpl

--- caseframesvc/src/main/java/uk/gov/ons/ctp/response/caseframe/service/impl/AddressServiceImpl.java
+++ caseframesvc/src/main/java/uk/gov/ons/ctp/response/caseframe/service/impl/AddressServiceImpl.java
@@ -18,24 +18,46 @@ import uk.gov.ons.ctp.response.caseframe.service.AddressService;
 @Slf4j
 public final class AddressServiceImpl implements AddressService {
 
-  public static final int INWARD_POSTCODE = 3;
-  public static final int POSTCODE_LENGTH = 8;
+  private static final int INWARD_POSTCODE = 3;
+  private static final int POSTCODE_LENGTH = 8;
 
+  /**
+   * Spring Data Repository for Address entities.
+   */
   @Inject
-  AddressRepository addressRepository;
+  private AddressRepository addressRepository;
 
+  /**
+   * Find Address entity by Unique Property Reference No.
+   *
+   * @param uprn A UPRN Long
+   * @return Address object or null
+   */
   @Override
   public Address findByUprn(final Long uprn) {
     log.debug("Entering findByUprn with {}", uprn);
     return addressRepository.findByUprn(uprn);
   }
 
+  /**
+   * Find Address entities by postcode.
+   *
+   * @param postcode A Postcode string
+   * @return List of Questionnaire entities or empty List
+   */
   @Override
   public List<Address> findByPostcode(final String postcode) {
     log.debug("Entering findByPostcode with {}", postcode);
     return addressRepository.findByPostcode(formatPostcode(postcode));
   }
 
+  /**
+   * Format a postcode, uppercase, pad to 8 characters allowing for different
+   * postcode formats.
+   *
+   * @param postcode A Postcode string
+   * @return Formatted postcode or null
+   */
   private String formatPostcode(final String postcode) {
     StringBuilder fullPostcode = null;
 
@@ -44,7 +66,7 @@ public final class AddressServiceImpl implements AddressService {
     if (trimmedPostcodeLength < POSTCODE_LENGTH) {
       // Pad spaces in centre of postcode
       String outwardPostCode = trimmedPostcode.substring(0, trimmedPostcodeLength - INWARD_POSTCODE);
-      String inwardPostCode = trimmedPostcode.substring(trimmedPostcodeLength - INWARD_POSTCODE, trimmedPostcodeLength);       
+      String inwardPostCode = trimmedPostcode.substring(trimmedPostcodeLength - INWARD_POSTCODE, trimmedPostcodeLength);
       fullPostcode = new StringBuilder(outwardPostCode);
       fullPostcode.append(String.format("%1$" + (POSTCODE_LENGTH - outwardPostCode.length()) + "s", inwardPostCode));
     } else {
