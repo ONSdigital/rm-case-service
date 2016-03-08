dd5f0c73869411ad5557fead701cab4a7c472bfe
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-03-08 16:24:44 +0000

    commenting out our only integration test in the responsemanagemnt project. These integration tests will now be under /fabric/integration-tests

--- caseframesvc/src/test/java/uk/gov/ons/ctp/response/caseframe/CaseFrameSvcIntegrationTest.java
+++ caseframesvc/src/test/java/uk/gov/ons/ctp/response/caseframe/CaseFrameSvcIntegrationTest.java
@@ -26,44 +26,44 @@ import uk.gov.ons.ctp.response.caseframe.service.impl.SurveyServiceImpl;
  * Example Integration Test
  *
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = uk.gov.ons.ctp.response.caseframe.CaseFrameSvcApplication.class)
-@WebAppConfiguration
-@IntegrationTest("server.port=8171")
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = uk.gov.ons.ctp.response.caseframe.CaseFrameSvcApplication.class)
+//@WebAppConfiguration
+//@IntegrationTest("server.port=8171")
 public class CaseFrameSvcIntegrationTest {
 
-  @Autowired
-  private ApplicationContext context;
-
-  private RestTemplate restTemplate = new TestRestTemplate();
-
-  /**
-   * hardwired disabling of liquibase
-   */
-  @BeforeClass
-  public static void switchOffLiquibase() {
-    Properties sysProp = System.getProperties();
-    sysProp.setProperty("liquibase.enabled", "false");
-  }
-
-  /**
-   * test example
-   */
-  @Test
-  public final void surveyEndpointTest() {
-
-    // To use real repository and go to DB simply remove setting of mock
-    // Repository below.
-    Survey survey = new Survey(1, "CTP", "Test case 1");
-
-    SurveyRepository mockRepo = Mockito.mock(SurveyRepository.class);
-    Mockito.when(mockRepo.findOne(1)).thenReturn(survey);
-    // Remove above to use DB rather than mock
-
-    SurveyServiceImpl service = context.getBean(SurveyServiceImpl.class);
-    service.setSurveyRepo(mockRepo);
-
-    ResponseEntity<Survey> entity = restTemplate.getForEntity("http://localhost:8171/surveys/1", Survey.class);
-    assertTrue(entity.getStatusCode().is2xxSuccessful());
-  }
+//  @Autowired
+//  private ApplicationContext context;
+//
+//  private RestTemplate restTemplate = new TestRestTemplate();
+//
+//  /**
+//   * hardwired disabling of liquibase
+//   */
+//  @BeforeClass
+//  public static void switchOffLiquibase() {
+//    Properties sysProp = System.getProperties();
+//    sysProp.setProperty("liquibase.enabled", "false");
+//  }
+//
+//  /**
+//   * test example
+//   */
+//  @Test
+//  public final void surveyEndpointTest() {
+//
+//    // To use real repository and go to DB simply remove setting of mock
+//    // Repository below.
+//    Survey survey = new Survey(1, "CTP", "Test case 1");
+//
+//    SurveyRepository mockRepo = Mockito.mock(SurveyRepository.class);
+//    Mockito.when(mockRepo.findOne(1)).thenReturn(survey);
+//    // Remove above to use DB rather than mock
+//
+//    SurveyServiceImpl service = context.getBean(SurveyServiceImpl.class);
+//    service.setSurveyRepo(mockRepo);
+//
+//    ResponseEntity<Survey> entity = restTemplate.getForEntity("http://localhost:8171/surveys/1", Survey.class);
+//    //assertTrue(entity.getStatusCode().is2xxSuccessful());
+//  }
 }
