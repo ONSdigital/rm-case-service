9e7886d1dcf4e39c904fc714c7cb495652b9e07b
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-04-06 16:05:02 +0100

    test client for Kirona amended

--- kirona-drs-gateway/src/main/java/uk/gov/ons/ctp/response/kirona/drs/message/FeedbackService.java
+++ kirona-drs-gateway/src/main/java/uk/gov/ons/ctp/response/kirona/drs/message/FeedbackService.java
@@ -1,5 +1,6 @@
 package uk.gov.ons.ctp.response.kirona.drs.message;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.integration.annotation.Publisher;
 import org.springframework.messaging.handler.annotation.Header;
 
@@ -8,9 +9,11 @@ import javax.inject.Named;
 import uk.gov.ons.ctp.response.action.message.feedback.ActionFeedback;
 
 @Named
+@Slf4j
 public class FeedbackService {
   @Publisher(channel="actionFeedbackOutbound")
-  public ActionFeedback sendRequest() {
+  public ActionFeedback sendFeedback() {
+    log.debug("Entering sendFeedback...");
     ActionFeedback actionFeedback = new ActionFeedback();
     // TODO Define attributes
     return actionFeedback;
--- kirona-drs-gateway/src/main/java/uk/gov/ons/ctp/response/kirona/drs/message/InstructionService.java
+++ kirona-drs-gateway/src/main/java/uk/gov/ons/ctp/response/kirona/drs/message/InstructionService.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import uk.gov.ons.ctp.response.action.message.instruction.ActionInstruction;
+import uk.gov.ons.ctp.response.action.message.instruction.ActionRequest;
+import uk.gov.ons.ctp.response.action.message.instruction.ActionRequests;
+
+import java.util.List;
 
 @Slf4j
 @MessageEndpoint
@@ -11,6 +15,15 @@ public class InstructionService {
   @ServiceActivator(inputChannel="actionInstructionTransformed")
   public void acceptInstruction(ActionInstruction instruction) {
     log.debug("Entering acceptInstruction with instruction {}", instruction);
-    // TODO From instruction, get the info required to call the Kirona WS
+    ActionRequests actionRequests = instruction.getActionRequests();
+    if (actionRequests != null) {
+      List<ActionRequest> actionRequestList = actionRequests.getActionRequests();
+      if (actionRequestList != null && !actionRequestList.isEmpty()) {
+        for (ActionRequest anActionRequest: actionRequestList) {
+          log.debug("dealing with actionRequest {}", anActionRequest);
+          //TODO
+        }
+      }
+    }
   }
 }
