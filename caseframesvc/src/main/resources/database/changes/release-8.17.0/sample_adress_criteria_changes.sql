b8f912fd5d0a1b77d9f8fafbdecf1ff802d1b109
Narinder Birk <narinder.birk@ons.gov.uk>
2016-05-13 13:22:09 +0100

    CTPA-378 - change address criteria on sample table

---
+++ caseframesvc/src/main/resources/database/changes/release-8.17.0/sample_adress_criteria_changes.sql
@@ -0,0 +1,8 @@
+﻿--update sample addresscriteria to use correct category values
+
+update caseframe.sample
+set addresscriteria = 'addresstype = ''CE'' and category = ''HOTEL'''
+where name = 'Hotels Guest Houses';
+update caseframe.sample
+set addresscriteria = 'addresstype = ''CE'' and category = ''CARE HOME'''
+where name = 'Care Homes';
