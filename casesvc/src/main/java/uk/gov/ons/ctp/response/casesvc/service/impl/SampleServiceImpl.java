10d4f382852622f272793925ea7837c41e625338
Martin Humphrey <martin.humphrey@ons.gov.uk>
2016-08-11 15:02:44 +0100

    CTPA--622 Case lifecycle event messages
    Increase settings for bulk creation of cases

--- casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/service/impl/SampleServiceImpl.java
+++ casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/service/impl/SampleServiceImpl.java
@@ -29,7 +29,7 @@ import uk.gov.ons.ctp.response.casesvc.service.SampleService;
 @Slf4j
 public final class SampleServiceImpl implements SampleService {
 
-  private static final int TRANSACTION_TIMEOUT = 30;
+  private static final int TRANSACTION_TIMEOUT = 120;
 
   @Inject
   private AppConfig appConfig;
