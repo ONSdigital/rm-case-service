861f4e05772a72f4af1ec85110aeb5c93e18e063
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-09-14 20:57:43 +0100

    CTPA-700 tempo commit on generating pdf

--- actionexporter/src/main/java/uk/gov/ons/ctp/response/action/export/service/TemplateInvestigation.java
+++ actionexporter/src/main/java/uk/gov/ons/ctp/response/action/export/service/TemplateInvestigation.java
@@ -1,6 +1,15 @@
 package uk.gov.ons.ctp.response.action.export.service;
 
 
+import fr.opensagres.xdocreport.converter.ConverterTypeTo;
+import fr.opensagres.xdocreport.converter.ConverterTypeVia;
+import fr.opensagres.xdocreport.converter.Options;
+import fr.opensagres.xdocreport.core.XDocReportException;
+import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
+import fr.opensagres.xdocreport.template.IContext;
+import fr.opensagres.xdocreport.template.TemplateEngineKind;
+import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -17,7 +26,7 @@ import java.util.Map;
  * Class to investigate best option for templating - CTPA-700
  */
 public class TemplateInvestigation {
-  public static void main(String[] args) throws IOException, TemplateException {
+  public static void main(String[] args) throws IOException, TemplateException, XDocReportException {
     /**
      * Step - Get the action requests from the MongoDB
      */
@@ -45,6 +54,10 @@ public class TemplateInvestigation {
     template.process(data, file);
     file.flush();
     file.close();
+
+    // Pdf example
+    OutputStream pdfOutput = new FileOutputStream("actionexporter/src/main/resources/forPrinter.pdf");
+    writeAsPdf(buildAMeActionRequest(1), pdfOutput);
   }
 
   /**
@@ -64,5 +77,36 @@ public class TemplateInvestigation {
     result.setActionType("testActionType");
     result.setIac("testIac");
     return result;
+  }
+
+  private static void writeAsPdf(ActionRequest actionRequest, OutputStream out)
+          throws IOException, XDocReportException {
+    InputStream in = getDefaultTemplate();
+
+    // Prepare the IXDocReport instance based on the template, using Freemarker template engine
+    IXDocReport report = XDocReportRegistry.getRegistry().loadReport(in, TemplateEngineKind.Freemarker);
+
+    // Define what we want to do (PDF file from ODF template)
+    Options options = Options.getTo(ConverterTypeTo.PDF).via(ConverterTypeVia.ODFDOM);
+
+    // Add properties to the context
+    IContext ctx = report.createContext();
+    ctx.put("actionRequest", actionRequest);
+    // TODO ctx.put("to", actionRequest.getTo());
+    // TODO ctx.put("sender", invoice.getInvoicer());
+
+    // instruct XDocReport to inspect InvoiceRow entity as well which is given as list and iterated in a table
+    FieldsMetadata metadata = report.createFieldsMetadata();
+    // TODO metadata.load("r", InvoiceRow.class, true);
+    // TODO ctx.put("r", invoice.getInvoiceRows());
+
+    // Write the PDF file to output stream
+    report.convert(ctx, options, out);
+    out.close();
+  }
+
+  private static InputStream getDefaultTemplate() {
+    // TODO modify this .odt so it matches our ActionRequest fields
+    return ActionRequest.class.getResourceAsStream("/tmpl.odt");
   }
 }
