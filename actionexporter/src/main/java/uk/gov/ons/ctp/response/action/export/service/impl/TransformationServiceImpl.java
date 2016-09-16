7f4c5eb98906e767285f69cd378f8d3531bf1cff
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-09-16 20:22:51 +0100

    CTPA-700 refactoring

--- actionexporter/src/main/java/uk/gov/ons/ctp/response/action/export/service/impl/TransformationServiceImpl.java
+++ actionexporter/src/main/java/uk/gov/ons/ctp/response/action/export/service/impl/TransformationServiceImpl.java
@@ -27,64 +27,72 @@ public class TransformationServiceImpl implements TransformationService {
 
   @Override
   public File fileMe(List<ActionRequestDocument> actionRequestDocumentList, String path) {
-    // Build the data model
-    Map<String, Object> root = new HashMap<String, Object>();
-    root.put("actionRequests", actionRequestDocumentList);
-
     File resultFile = new File(path);
+    Writer fileWriter = null;
     try {
-      // TODO get templates from db: follow http://www.nurkiewicz.com/2010/01/writing-custom-freemarker-template.html
-      Configuration cfg = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_25);
-      File templateDirectory = resourceLoader.getResource("classpath:templates").getFile();
-      cfg.setDirectoryForTemplateLoading(templateDirectory);  // non-file-system sources are possible too: see setTemplateLoader();
-      cfg.setDefaultEncoding(UTF8.name());
-      cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-      // Don't log exceptions inside FreeMarker that it will thrown at you anyway:
-      cfg.setLogTemplateExceptions(false);
-
-      Template template = cfg.getTemplate("csvExport.ftl"); // Configuration caches Template instances
-
-      // File output
-      Writer fileWriter = new FileWriter(resultFile);
-      template.process(root, fileWriter);
-      fileWriter.close();
+      fileWriter = new FileWriter(resultFile);
+      Template template = giveMeTemplate();
+      template.process(buildDataModel(actionRequestDocumentList), fileWriter);
     } catch (IOException e) {
       log.error("IOException thrown while templating for file...", e.getMessage());
     } catch (TemplateException f) {
       log.error("TemplateException thrown while templating for file...", f.getMessage());
+    } finally {
+      if (fileWriter != null) {
+        try {
+          fileWriter.close();
+        } catch (IOException e) {
+          log.error("IOException thrown while closing the file writer...", e.getMessage());
+        }
+      }
     }
+
     return resultFile;
   }
 
   @Override
   public ByteArrayOutputStream streamMe(List<ActionRequestDocument> actionRequestDocumentList) {
-    // Build the data model
-    Map<String, Object> root = new HashMap<String, Object>();
-    root.put("actionRequests", actionRequestDocumentList);
-
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    Writer outputStreamWriter = null;
     try {
-      // TODO get templates from db: follow http://www.nurkiewicz.com/2010/01/writing-custom-freemarker-template.html
-      Configuration cfg = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_25);
-      File templateDirectory = resourceLoader.getResource("classpath:templates").getFile();
-      cfg.setDirectoryForTemplateLoading(templateDirectory);  // non-file-system sources are possible too: see setTemplateLoader();
-      cfg.setDefaultEncoding(UTF8.name());
-      cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-      // Don't log exceptions inside FreeMarker that it will thrown at you anyway:
-      cfg.setLogTemplateExceptions(false);
-
-      Template template = cfg.getTemplate("csvExport.ftl"); // Configuration caches Template instances
-
-      // Streaming
-      Writer outputStreamWriter = new OutputStreamWriter(outputStream);
-      template.process(root, outputStreamWriter);
+      outputStreamWriter = new OutputStreamWriter(outputStream);
+      Template template = giveMeTemplate();
+      template.process(buildDataModel(actionRequestDocumentList), outputStreamWriter);
       outputStreamWriter.close();
     } catch (IOException e) {
-      log.error("IOException thrown while templating for file...", e.getMessage());
+      log.error("IOException thrown while templating for stream...", e.getMessage());
     } catch (TemplateException f) {
-      log.error("TemplateException thrown while templating for file...", f.getMessage());
+      log.error("TemplateException thrown while templating for stream...", f.getMessage());
+    } finally {
+      if (outputStreamWriter != null) {
+        try {
+          outputStreamWriter.close();
+        } catch (IOException e) {
+          log.error("IOException thrown while closing the output stream writer...", e.getMessage());
+        }
+      }
     }
 
     return outputStream;
+  }
+
+  private Template giveMeTemplate() throws IOException {
+    // TODO get templates from db: follow http://www.nurkiewicz.com/2010/01/writing-custom-freemarker-template.html
+    Configuration cfg = new freemarker.template.Configuration(freemarker.template.Configuration.VERSION_2_3_25);
+    File templateDirectory = resourceLoader.getResource("classpath:templates").getFile();
+    cfg.setDirectoryForTemplateLoading(templateDirectory);  // non-file-system sources are possible too: see setTemplateLoader();
+    cfg.setDefaultEncoding(UTF8.name());
+    cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+    // Don't log exceptions inside FreeMarker that it will thrown at you anyway:
+    cfg.setLogTemplateExceptions(false);
+
+    Template template = cfg.getTemplate("csvExport.ftl"); // Configuration caches Template instances
+    return template;
+  }
+
+  private Map<String, Object> buildDataModel(List<ActionRequestDocument> actionRequestDocumentList) {
+    Map<String, Object> result = new HashMap<String, Object>();
+    result.put("actionRequests", actionRequestDocumentList);
+    return result;
   }
 }
