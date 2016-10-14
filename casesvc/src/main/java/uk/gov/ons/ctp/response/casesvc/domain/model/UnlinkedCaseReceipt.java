ceab2a8ec2d81c9a34be986fb30a05422cb639b0
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-10-14 14:49:13 +0100

    CTPA-806 db script for new table unlinkedcasereceipt

---
+++ casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/domain/model/UnlinkedCaseReceipt.java
@@ -0,0 +1,37 @@
+package uk.gov.ons.ctp.response.casesvc.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.ons.ctp.response.casesvc.message.feedback.InboundChannel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+/**
+ * Domain model object.
+ */
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "unlinkedcasereceipt", schema = "casesvc")
+public class UnlinkedCaseReceipt {
+  @Id
+  @Column(name = "caseref")
+  private String caseRef;
+
+  @Column(name = "inboundchannel")
+  @Enumerated(EnumType.STRING)
+  private InboundChannel inboundChannel;
+
+  @Column(name = "respondedatetime")
+  private Timestamp responseDateTime;
+}
