7ce425df19165b6a059633263950ff3ec78d7ae4
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-10-19 15:26:48 +0100

    CTPA-537 refactoring the si flow

--- casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/message/CaseReceiptProcessErrorReceiver.java
+++ casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/message/CaseReceiptProcessErrorReceiver.java
@@ -1,14 +1,14 @@
 package uk.gov.ons.ctp.response.casesvc.message;
 
-import org.springframework.messaging.MessageHandlingException;
+import org.springframework.messaging.Message;
 
 /**
- * The reader of messages put on channel caseReceiptProcessError
+ * The reader of messages put on channel caseReceiptProcessErrorFailedMsgOnly
  */
 public interface CaseReceiptProcessErrorReceiver {
   /**
-   * To process exceptions put on channel caseReceiptProcessError
-   * @param exception the exception to process
+   * To process messages put on channel caseReceiptProcessErrorFailedMsgOnly
+   * @param message the message to process
    */
-  void process(MessageHandlingException exception);
+  void process(Message<?> message) ;
 }
