e39e61e398e4498265a967d232876750a045874f
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-10-20 13:58:37 +0100

    CTPA-357 tidy up checkstyle issues

--- casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/service/UnlinkedCaseReceiptService.java
+++ casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/service/UnlinkedCaseReceiptService.java
@@ -3,6 +3,14 @@ package uk.gov.ons.ctp.response.casesvc.service;
 import uk.gov.ons.ctp.common.service.CTPService;
 import uk.gov.ons.ctp.response.casesvc.domain.model.UnlinkedCaseReceipt;
 
+/**
+ * The service to deal with unlinkedCaseReceipts.
+ */
 public interface UnlinkedCaseReceiptService extends CTPService {
+  /**
+   * To store an unlinkedCaseReceipt
+   * @param unlinkedCaseReceipt to be stored
+   * @return the stored unlinkedCaseReceipt
+   */
   UnlinkedCaseReceipt createUnlinkedCaseReceipt(UnlinkedCaseReceipt unlinkedCaseReceipt);
 }
--- casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/service/impl/UnlinkedCaseReceiptServiceImpl.java
+++ casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/service/impl/UnlinkedCaseReceiptServiceImpl.java
@@ -7,12 +7,20 @@ import uk.gov.ons.ctp.response.casesvc.domain.model.UnlinkedCaseReceipt;
 import uk.gov.ons.ctp.response.casesvc.domain.repository.UnlinkedCaseReceiptRepository;
 import uk.gov.ons.ctp.response.casesvc.service.UnlinkedCaseReceiptService;
 
+/**
+ * The service to deal with unlinkedCaseReceipts.
+ */
 @Named
 public class UnlinkedCaseReceiptServiceImpl implements UnlinkedCaseReceiptService {
 
   @Inject
   private UnlinkedCaseReceiptRepository unlinkedCaseReceiptRepository;
 
+  /**
+   * To store an unlinkedCaseReceipt
+   * @param unlinkedCaseReceipt to be stored
+   * @return the stored unlinkedCaseReceipt
+   */
   @Override
   public UnlinkedCaseReceipt createUnlinkedCaseReceipt(UnlinkedCaseReceipt unlinkedCaseReceipt) {
     return unlinkedCaseReceiptRepository.saveAndFlush(unlinkedCaseReceipt);
