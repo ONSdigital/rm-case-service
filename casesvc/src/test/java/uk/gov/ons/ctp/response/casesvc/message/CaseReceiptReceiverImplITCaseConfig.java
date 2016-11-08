3286f24e43a68d975088fae8147580185ef13bcc
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-11-08 12:02:45 +0000

    CTPA-806 tidy up

--- casesvc/src/test/java/uk/gov/ons/ctp/response/casesvc/message/CaseReceiptReceiverImplITCaseConfig.java
+++ casesvc/src/test/java/uk/gov/ons/ctp/response/casesvc/message/CaseReceiptReceiverImplITCaseConfig.java
@@ -1,9 +1,15 @@
 package uk.gov.ons.ctp.response.casesvc.message;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootConfiguration
 @ImportResource(locations = { "classpath:CaseReceiptReceiverImplITCase-context.xml" })
 public class CaseReceiptReceiverImplITCaseConfig {
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
 }
