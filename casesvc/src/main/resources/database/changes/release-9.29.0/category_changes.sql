652d48ca1f2eac874dca6639ebac57b5b9192297
Narinder Birk <narinder.birk@ons.gov.uk>
2016-11-14 11:54:18 +0000

    CTPA- 830 - new categories to support capture of field outcome code

--- casesvc/src/main/resources/database/changes/release-9.29.0/category_changes.sql
+++ casesvc/src/main/resources/database/changes/release-9.29.0/category_changes.sql
@@ -1,4 +1,4 @@
-﻿--category details for action_completed_desactivated and action_completed_disabled
+﻿--category details for action_completed_deactivated and action_completed_disabled
 
 INSERT INTO casesvc.category (name, longdescription, eventtype, manual, role, generatedactiontype, "group", shortdescription) VALUES ('ACTION_COMPLETED_DISABLED', 'Action Completed Disabled', 'DISABLED', false, NULL, NULL, NULL, 'Action Completed Disabled');
 INSERT INTO casesvc.category (name, longdescription, eventtype, manual, role, generatedactiontype, "group", shortdescription) VALUES ('ACTION_COMPLETED_DEACTIVATED', 'Action Completed Deactivated', 'DEACTIVATED', false, NULL, NULL, NULL, 'Action Completed Deactivated');
