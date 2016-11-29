489cc98e7f431802ecc5cf75ee09277da78989d5
Narinder Birk <narinder.birk@ons.gov.uk>
2016-11-29 13:20:32 +0000

    CTPA- 945 - Creat escalated refusal category

---
+++ casesvc/src/main/resources/database/changes/release-9.30.0/category_escalation_changes.sql
@@ -0,0 +1,12 @@
+﻿--category changes for escalated alls and refusals 
+
+UPDATE CASESVC.CATEGORY
+SET role = 'collect-csos, collect-admins'
+WHERE NAME IN ('FIELD_COMPLAINT_ESCALATED','FIELD_EMERGENCY_ESCALATED','GENERAL_COMPLAINT_ESCALATED','GENERAL_ENQUIRY_ESCALATED');
+
+UPDATE CASESVC.CATEGORY
+SET role = 'collect-csos, collect-admins'
+WHERE NAME = 'REFUSAL';
+
+INSERT INTO casesvc.category (name, longdescription, eventtype, manual, role, generatedactiontype, "group", shortdescription, newcaserespondenttype, oldcaserespondenttype) VALUES ('ESCALATED_REFUSAL', 'Escalated Refusal', 'DEACTIVATED', true, 'collect-field-escalate, collect-general-escalate, collect-admins', NULL, 'general', 'Escalated Refusal', NULL, NULL);
+
