9f693b1ae5b07bad1ff8a2963ce02f39bd70801a
Sarah Radford <sarah.radford@ons.gov.uk>
2016-12-05 11:21:15 +0000

    CTPA-776 Remove space character in filename and correct address join for sample size in base view

---
+++ casesvc/src/main/resources/database/changes/release-9.30.0/copy_csv_correct_space.sql
@@ -0,0 +1,20 @@
+-- Function: casesvc.copy_csv(character varying, character varying, character varying)
+
+-- DROP FUNCTION casesvc.copy_csv(character varying, character varying, character varying);
+
+CREATE OR REPLACE FUNCTION casesvc.copy_csv(p_sql character varying, p_directory character varying, p_filename character varying)
+  RETURNS boolean AS
+$BODY$
+DECLARE    
+BEGIN
+ EXECUTE 'COPY (' || p_sql || ') TO ''' || p_directory || p_filename || '.csv''' || 'DELIMITER '','' CSV HEADER';
+                    
+ PERFORM casesvc.logmessage(p_messagetext := 'Command - ' || 'COPY (' || p_sql || ') TO ' || p_directory || p_filename || '.csv DELIMITER '','' CSV HEADER'
+                           ,p_jobid := 0   
+                           ,p_messagelevel := 'INFO'
+                           ,p_functionname := 'casesvc.copy_csv');  
+ RETURN true;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE SECURITY DEFINER
+  COST 100;
