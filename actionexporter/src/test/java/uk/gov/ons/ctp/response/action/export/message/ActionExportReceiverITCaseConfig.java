d0d036b63e07990502df895ee72567d136a59a7d
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-12-05 15:21:43 +0000

    CTPA-954 renaming actionexporter to actionexportersvc

--- actionexporter/src/test/java/uk/gov/ons/ctp/response/action/export/message/ActionExportReceiverITCaseConfig.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.ons.ctp.response.action.export.message;
-
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.annotation.ImportResource;
-
-@SpringBootConfiguration
-@ImportResource(locations = { "classpath:springintegration/ActionExportReceiverITCase-context.xml" })
-public class ActionExportReceiverITCaseConfig {
-}
--- actionexporter/src/test/resources/templates/freemarker/initialPrint.ftl
+++
@@ -1,3 +0,0 @@
-<#list actionRequests as actionRequest>
-${actionRequest.iac}|${(actionRequest.caseRef)!}|${(actionRequest.address.organisationName)!}||||${(actionRequest.address.line1)!}|${(actionRequest.address.line2)!}|${(actionRequest.address.locality)!}|${(actionRequest.address.townName)!}|${(actionRequest.address.postcode)!}
-</#list>
