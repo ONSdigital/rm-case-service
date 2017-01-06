ab28b659038c64349a8b3f3472530ae664e8dcdf
Edward Stevens <edward.stevens@ons.gov.uk>
2017-01-06 16:07:22 +0000

    Removed unused import from ReportEndpoint

--- casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/endpoint/ReportEndpoint.java
+++ casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/endpoint/ReportEndpoint.java
@@ -2,7 +2,6 @@ package uk.gov.ons.ctp.response.casesvc.endpoint;
 
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import javax.inject.Inject;
