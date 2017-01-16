52cda675e5d415caf2880176d7e1ab768ee2dfd5
Philippe Brossier <philippe.brossier@ons.gov.uk>
2017-01-16 15:44:13 +0000

    CTPA-1048 response time now stored correctly in table response

--- casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/service/CaseService.java
+++ casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/service/CaseService.java
@@ -1,5 +1,6 @@
 package uk.gov.ons.ctp.response.casesvc.service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import uk.gov.ons.ctp.common.service.CTPService;
@@ -54,15 +55,26 @@ public interface CaseService extends CTPService {
   List<CaseEvent> findCaseEventsByCaseId(Integer caseId);
 
   /**
-   * Create a CaseEvent from the details provided in the passesd CaseEvent. Some events will also as a side effect
+   * Create a CaseEvent from the details provided in the passed CaseEvent. Some events will also as a side effect
    * create a new case - if so the details must be provided in the newCase argument, otherwise it may remain null.
    * If the newCase is passed it must also contain the contact details for the new case.
+   *
    * @param caseEvent CaseEvent to be created
    * @param newCase optional case object containing partial details of the case to be created by this event.
    * @return the created CaseEvent
    */
   CaseEvent createCaseEvent(CaseEvent caseEvent, Case newCase);
-  
+
+  /**
+   * Create a CaseEvent for the specific scenario of an incoming CaseReceipt (sent by the SDX Gateway and containing the
+   * responseDateTime of the online/paper response)
+   *
+   * @param caseEvent CaseEvent to be created
+   * @param newCase optional case object containing partial details of the case to be created by this event.
+   * @param timestamp timestamp equals to the incoming CaseReceipt's responseDateTime
+   * @return the created CaseEvent
+   */
+  CaseEvent createCaseEvent(CaseEvent caseEvent, Case newCase, Timestamp timestamp);
 
   /**
    * Not sure this is the best place for this method, but .. several parts of case svc need to build a 
