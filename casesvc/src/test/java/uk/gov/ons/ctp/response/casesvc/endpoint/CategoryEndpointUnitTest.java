886dd96d006c23851c1a6d233b2be44a147fc920
Philippe Brossier <brossierp@gmail.com>
2017-04-24 14:54:16 +0100

    ctpa1187 category endpoint now tested

--- casesvc/src/test/java/uk/gov/ons/ctp/response/casesvc/endpoint/CategoryEndpointUnitTest.java
+++ casesvc/src/test/java/uk/gov/ons/ctp/response/casesvc/endpoint/CategoryEndpointUnitTest.java
@@ -2,31 +2,12 @@ package uk.gov.ons.ctp.response.casesvc.endpoint;
 
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.Is.isA;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.ons.ctp.common.MvcHelper.getJson;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.ADMIN_ROLE;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY1_GROUP;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY1_LONG_DESC;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY1_MANUAL;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY1_ROLE;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY1_SHORT_DESC;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY1_TYPE;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY2_GROUP;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY2_LONG_DESC;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY2_MANUAL;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY2_ROLE;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY2_SHORT_DESC;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY2_TYPE;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY3_GROUP;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY3_LONG_DESC;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY3_MANUAL;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY3_ROLE;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY3_SHORT_DESC;
-import static uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory.CATEGORY3_TYPE;
 import static uk.gov.ons.ctp.response.casesvc.utility.MockMvcControllerAdviceHelper.mockAdviceFor;
 
 import ma.glasnost.orika.MapperFacade;
@@ -37,7 +18,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
-import org.springframework.http.HttpStatus;
 
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -47,7 +27,6 @@ import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.response.casesvc.CaseSvcBeanMapper;
 import uk.gov.ons.ctp.response.casesvc.domain.model.Category;
 import uk.gov.ons.ctp.response.casesvc.service.CategoryService;
-import uk.gov.ons.ctp.response.casesvc.utility.MockCategoryServiceFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,8 +47,21 @@ public final class CategoryEndpointUnitTest {
   private MapperFacade mapperFacade = new CaseSvcBeanMapper();
 
   private MockMvc mockMvc;
+  private List<Category> categoryResults;
 
   private static final String CATEGORY_UNKNOWN = "Felix The Cat";
+  private static final String CATEGORY1_NAME = "ACCESSIBILITY_MATERIALS";
+  private static final String CATEGORY2_NAME = "ACTION_CANCELLATION_COMPLETED";
+  private static final String CATEGORY3_NAME = "ACTION_CANCELLATION_CREATED";
+  private static final String CATEGORY1_SHORT_DESC = "Accessibility Materials";
+  private static final String CATEGORY2_SHORT_DESC = "Action Cancellation Completed";
+  private static final String CATEGORY3_SHORT_DESC = "Action Cancellation Created";
+  private static final String CATEGORY1_LONG_DESC = "Accessibility Materials Blah";
+  private static final String CATEGORY2_LONG_DESC = "Action Cancellation Completed Blah";
+  private static final String CATEGORY3_LONG_DESC = "Action Cancellation Created Blah";
+  private static final String CATEGORY1_ROLE = "collect-csos, collect-admins";
+  private static final String ADMIN_ROLE = "collect-admins";
+  private static final String CATEGORY1_GROUP = "general";
 
   @Before
   public void setUp() throws Exception {
@@ -79,6 +71,8 @@ public final class CategoryEndpointUnitTest {
             .standaloneSetup(categoryEndpoint)
             .setHandlerExceptionResolvers(mockAdviceFor(RestExceptionHandler.class))
             .build();
+
+    this.categoryResults = FixtureHelper.loadClassFixtures(Category[].class);
   }
 
   /**
@@ -101,7 +95,7 @@ public final class CategoryEndpointUnitTest {
    */
   @Test
   public void findCategoriesFoundNoRoleSpecified() throws Exception {
-    when(categoryService.findCategories(null, null)).thenReturn(FixtureHelper.loadClassFixtures(Category[].class));
+    when(categoryService.findCategories(null, null)).thenReturn(categoryResults);
 
     ResultActions actions = mockMvc.perform(getJson("/categories"));
 
@@ -109,33 +103,35 @@ public final class CategoryEndpointUnitTest {
     actions.andExpect(handler().handlerType(CategoryEndpoint.class));
     actions.andExpect(handler().methodName("findCategories"));
     actions.andExpect(jsonPath("$", Matchers.hasSize(3)));
-    // TODO assert values for each param in 3
-
-//        .assertStringListInBody("$..name", CATEGORY1_TYPE, CATEGORY2_TYPE, CATEGORY3_TYPE)
-//        .assertStringListInBody("$..longDescription", CATEGORY1_LONG_DESC, CATEGORY2_LONG_DESC, CATEGORY3_LONG_DESC)
-//        .assertStringListInBody("$..shortDescription", CATEGORY1_SHORT_DESC, CATEGORY2_SHORT_DESC, CATEGORY3_SHORT_DESC)
-//        .assertStringListInBody("$..role", CATEGORY1_ROLE, CATEGORY2_ROLE, CATEGORY3_ROLE)
-//        .assertStringListInBody("$..group", CATEGORY1_GROUP, CATEGORY2_GROUP, CATEGORY3_GROUP)
-//        .assertBooleanListInBody("$..manual", CATEGORY1_MANUAL, CATEGORY2_MANUAL, CATEGORY3_MANUAL)
-//        .andClose();
+    actions.andExpect(jsonPath("$[*].group", containsInAnyOrder(CATEGORY1_GROUP, null, null)));
+    actions.andExpect(jsonPath("$[*].name", containsInAnyOrder(CATEGORY1_NAME, CATEGORY2_NAME, CATEGORY3_NAME)));
+    actions.andExpect(jsonPath("$[*].longDescription", containsInAnyOrder(CATEGORY1_LONG_DESC, CATEGORY2_LONG_DESC, CATEGORY3_LONG_DESC)));
+    actions.andExpect(jsonPath("$[*].shortDescription", containsInAnyOrder(CATEGORY1_SHORT_DESC, CATEGORY2_SHORT_DESC, CATEGORY3_SHORT_DESC)));
+    actions.andExpect(jsonPath("$[*].role", containsInAnyOrder(CATEGORY1_ROLE, null, null)));
+    actions.andExpect(jsonPath("$[*].manual", containsInAnyOrder(true, false, false)));
   }
-//
-//  /**
-//   * A test
-//   */
-//  @Test
-//  public void findCategoriesFoundAdminRoleSpecified() {
-//    String serviceUrl = String.format("/categories?role=%s", ADMIN_ROLE);
-//    with(serviceUrl)
-//        .assertResponseCodeIs(HttpStatus.OK)
-//        .assertArrayLengthInBodyIs(1)
-//        .assertStringListInBody("$..name", CATEGORY1_TYPE)
-//        .assertStringListInBody("$..longDescription", CATEGORY1_LONG_DESC)
-//        .assertStringListInBody("$..shortDescription", CATEGORY1_SHORT_DESC)
-//        .assertStringListInBody("$..role", ADMIN_ROLE)
-//        .assertStringListInBody("$..group", CATEGORY1_GROUP)
-//        .assertBooleanListInBody("$..manual", CATEGORY1_MANUAL)
-//        .andClose();
-//  }
+
+  /**
+   * A test
+   */
+  @Test
+  public void findCategoriesFoundAdminRoleSpecified() throws Exception {
+    List<Category> results = new ArrayList<>();
+    results.add(categoryResults.get(0));
+    when(categoryService.findCategories(ADMIN_ROLE, null)).thenReturn(results);
+
+    ResultActions actions = mockMvc.perform(getJson(String.format("/categories?role=%s", ADMIN_ROLE)));
+
+    actions.andExpect(status().isOk());
+    actions.andExpect(handler().handlerType(CategoryEndpoint.class));
+    actions.andExpect(handler().methodName("findCategories"));
+    actions.andExpect(jsonPath("$", Matchers.hasSize(1)));
+    actions.andExpect(jsonPath("$[0].group", is(CATEGORY1_GROUP)));
+    actions.andExpect(jsonPath("$[0].name", is(CATEGORY1_NAME)));
+    actions.andExpect(jsonPath("$[0].longDescription", is(CATEGORY1_LONG_DESC)));
+    actions.andExpect(jsonPath("$[0].shortDescription", is(CATEGORY1_SHORT_DESC)));
+    actions.andExpect(jsonPath("$[0].role", is(CATEGORY1_ROLE)));
+    actions.andExpect(jsonPath("$[0].manual", is(true)));
+  }
 
 }
