fa15532bbd593e521da7398aeb1ad1e9a6dbf16e
Philippe Brossier <brossierp@gmail.com>
2017-06-06 14:43:37 +0100

    CTPA-1274 now returning case group details as part of a get on cases

---
+++ casesvc-api/src/main/java/uk/gov/ons/ctp/response/casesvc/representation/CaseDetailsDTO.java
@@ -0,0 +1,48 @@
+package uk.gov.ons.ctp.response.casesvc.representation;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class CaseDetailsDTO {
+    /**
+     * enum for case state
+     */
+    public enum CaseState {
+        ACTIONABLE, INACTIONABLE, REPLACEMENT_INIT, SAMPLED_INIT;
+    }
+
+    /**
+     * enum for Case event
+     */
+    public enum CaseEvent {
+        ACTIVATED, DEACTIVATED, DISABLED, HOUSEHOLD_PAPER_REQUESTED, INDIVIDUAL_RESPONSE_REQUESTED, REPLACED
+    }
+
+    private UUID id;
+    private CaseDTO.CaseState state;
+    private String iac;
+    private String caseRef;
+
+    private UUID actionPlanId;
+    private UUID caseGroupId;
+    private UUID collectionInstrumentId;
+    private UUID partyId;
+
+    private String sampleUnitType;
+    private String sampleUnitRef;
+
+    private String createdBy;
+    private Date createdDateTime;
+
+    private ResponseDTO[] responses;
+
+    private CaseGroupDTO caseGroup;
+}
--- casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/CaseSvcBeanMapper.java
+++ casesvc/src/main/java/uk/gov/ons/ctp/response/casesvc/CaseSvcBeanMapper.java
@@ -10,11 +10,7 @@ import uk.gov.ons.ctp.response.casesvc.domain.model.CaseEvent;
 import uk.gov.ons.ctp.response.casesvc.domain.model.CaseGroup;
 import uk.gov.ons.ctp.response.casesvc.domain.model.Category;
 import uk.gov.ons.ctp.response.casesvc.domain.model.Response;
-import uk.gov.ons.ctp.response.casesvc.representation.CaseDTO;
-import uk.gov.ons.ctp.response.casesvc.representation.CaseEventDTO;
-import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupDTO;
-import uk.gov.ons.ctp.response.casesvc.representation.CategoryDTO;
-import uk.gov.ons.ctp.response.casesvc.representation.ResponseDTO;
+import uk.gov.ons.ctp.response.casesvc.representation.*;
 
 /**
  * The bean mapper that maps to/from DTOs and JPA entity types.
@@ -24,14 +20,20 @@ import uk.gov.ons.ctp.response.casesvc.representation.ResponseDTO;
 public class CaseSvcBeanMapper extends ConfigurableMapper {
 
   /**
-   * Setup the mapper for all of our beans. Only fields having non  identical names need
-   * mapping if we also use byDefault() following.
+   * Setup the mapper for all of our beans. Only fields having non identical names need mapping if we also use
+   * byDefault() following.
+   *
    * @param factory the factory to which we add our mappings
    */
   protected final void configure(final MapperFactory factory) {
 
     factory
             .classMap(Case.class, CaseDTO.class)
+            .byDefault()
+            .register();
+
+    factory
+            .classMap(Case.class, CaseDetailsDTO.class)
             .byDefault()
             .register();
 
