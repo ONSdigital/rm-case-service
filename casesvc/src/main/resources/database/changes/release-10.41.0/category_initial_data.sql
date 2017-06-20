4d84ce1e014df82c1a79314d25e9929c3581b1ef
Philippe Brossier <brossierp@gmail.com>
2017-06-20 09:42:16 +0100

    CTPA-1335 correcting typos

--- casesvc/src/main/resources/database/changes/release-10.41.0/category_initial_data.sql
+++ casesvc/src/main/resources/database/changes/release-10.41.0/category_initial_data.sql
@@ -8,7 +8,7 @@ ALTER TABLE casesvc.category ALTER COLUMN oldcasesampleunittypes SET NOT NULL;
 
 INSERT INTO casesvc.category (categorypk, shortdescription, longdescription, eventtype, role, generatedactiontype, "group", oldcasesampleunittypes, newcasesampleunittype, recalccollectioninstrument) VALUES ('CASE_CREATED', 'Case Created', 'Case Created', NULL, NULL, NULL, NULL,'B,BI',NULL,  NULL);
 
-INSERT INTO casesvc.category (categorypk, shortdescription, longdescription, eventtype, role, generatedactiontype, "group", oldcasesampleunittypes, newcasesampleunittype, recalccollectioninstrument) VALUES ('RESPONDENT_ACCOUNT_CREATED', 'ç', 'Account created for respondent', 'ACCOUNT_CREATED', NULL, NULL, NULL, 'B', NULL,  NULL);
+INSERT INTO casesvc.category (categorypk, shortdescription, longdescription, eventtype, role, generatedactiontype, "group", oldcasesampleunittypes, newcasesampleunittype, recalccollectioninstrument) VALUES ('RESPONDENT_ACCOUNT_CREATED', 'Account created for respondent', 'Account created for respondent', 'ACCOUNT_CREATED', NULL, NULL, NULL, 'B', NULL,  NULL);
 
 INSERT INTO casesvc.category (categorypk, shortdescription, longdescription, eventtype, role, generatedactiontype, "group", oldcasesampleunittypes, newcasesampleunittype, recalccollectioninstrument) VALUES ('ACCESS_CODE_AUTHENTICATION_ATTEMPT', 'Access Code authentication attempted', 'Access Code authentication atttempted', NULL, NULL, NULL, NULL, 'B',NULL,  NULL);
 
@@ -22,7 +22,7 @@ INSERT INTO casesvc.category (categorypk, shortdescription, longdescription, eve
 
 INSERT INTO casesvc.category (categorypk, shortdescription, longdescription, eventtype, role, generatedactiontype, "group", oldcasesampleunittypes, newcasesampleunittype, recalccollectioninstrument) VALUES ('UNSUCCESSFUL_RESPONSE_UPLOAD', 'Unsuccessful Response Upload', 'Unsuccessful Response Upload', NULL, NULL, NULL, NULL, 'BI', NULL, NULL);
 
-INSERT INTO casesvc.category (categorypk, shortdescription, longdescription, eventtype, role, generatedactiontype, "group", oldcasesampleunittypes, newcasesampleunittype, recalccollectioninstrument) VALUES ('ENROLMENT_CODE_VERIFIED', 'Enrolement code verified', 'Enrolement code verified', NULL, NULL, NULL, NULL, 'B', NULL, NULL);
+INSERT INTO casesvc.category (categorypk, shortdescription, longdescription, eventtype, role, generatedactiontype, "group", oldcasesampleunittypes, newcasesampleunittype, recalccollectioninstrument) VALUES ('ENROLMENT_CODE_VERIFIED', 'Enrolment code verified', 'Enrolment code verified', NULL, NULL, NULL, NULL, 'B', NULL, NULL);
 
 INSERT INTO casesvc.category (categorypk, shortdescription, longdescription, eventtype, role, generatedactiontype, "group", oldcasesampleunittypes, newcasesampleunittype, recalccollectioninstrument) VALUES ('SUCCESSFUL_RESPONSE_UPLOAD', 'Successful Response Upload', 'Successful Response Upload', 'DISABLED',  NULL, NULL, NULL, 'BI', NULL, NULL);
 
