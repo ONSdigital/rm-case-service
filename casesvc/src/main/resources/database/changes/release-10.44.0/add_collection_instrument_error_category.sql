939277fce5d266738965fc5878ea67ebd94059cc
John Topley <john.topley@ons.gov.uk>
2017-07-27 14:45:41 +0100

    CTPA-1470 Added Collection Instrument Error case event category

---
+++ casesvc/src/main/resources/database/changes/release-10.44.0/add_collection_instrument_error_category.sql
@@ -0,0 +1,2 @@
+INSERT INTO casesvc.category(categorypk,shortdescription,longdescription, oldcasesampleunittypes)
+VALUES ('COLLECTION_INSTRUMENT_ERROR','Collection Instrument Error','Collection Instrument Error','BI')
