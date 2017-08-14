25e127037e840d4e079585358421fac50910b830
Edward Stevens <edward.stevens@ons.gov.uk>
2017-08-14 16:36:20 +0100

    Add RESPONDENT_EMAIL_AMENDED category

---
+++ src/main/resources/database/changes/release-10.45.0/add_respondent_email_amended_category.sql
@@ -0,0 +1,2 @@
+INSERT INTO casesvc.category(categorypk,shortdescription,longdescription, oldcasesampleunittypes)
+VALUES ('RESPONDENT_EMAIL_AMENDED','Respondent Email Amended','Respondent Email Amended','BI')
