1eb5bbb759ebc7524d88b6490d70d5f81e952dcf
Narinder Birk <narinderbirk@ip-172-29-33-27.eu-west-1.compute.internal>
2017-09-13 12:00:06 +0100

    CTPA - 1583 - Reports are inconsistenly named

---
+++ src/main/resources/database/changes/release-10.47.0/removal_of_report_from_displayname.sql
@@ -0,0 +1,7 @@
+
+UPDATE casesvc.reporttype
+SET displayname = 'Case Events' WHERE reporttypepk = 'CASE_EVENTS';
+
+
+UPDATE casesvc.reporttype
+SET displayname = 'Response Chasing' WHERE reporttypepk = 'RESPONSE_CHASING';
