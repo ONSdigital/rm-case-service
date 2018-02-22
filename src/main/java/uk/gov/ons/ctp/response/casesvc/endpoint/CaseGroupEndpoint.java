e775fbcb8131b2adf9ecc132a6e13629c87a5107
liamtoozer <liam.toozer@gmail.com>
2018-02-22 09:52:08 +0000

    Allow group status transitioning via API
    * Add API to get valid transitions and the events required to trigger
    * Add API to transition
    * Refactor group status transition from CaseServiceImpl to
      CaseGroupService

--- src/main/java/uk/gov/ons/ctp/response/casesvc/endpoint/CaseGroupEndpoint.java
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/endpoint/CaseGroupEndpoint.java
@@ -1,20 +1,22 @@
 package uk.gov.ons.ctp.response.casesvc.endpoint;
 
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFacade;
 import uk.gov.ons.ctp.common.endpoint.CTPEndpoint;
 import uk.gov.ons.ctp.common.error.CTPException;
+import uk.gov.ons.ctp.common.state.StateTransitionManager;
 import uk.gov.ons.ctp.response.casesvc.domain.model.CaseGroup;
 import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupDTO;
+import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupStatus;
+import uk.gov.ons.ctp.response.casesvc.representation.CategoryDTO;
 import uk.gov.ons.ctp.response.casesvc.service.CaseGroupService;
 
 /**
@@ -29,6 +31,9 @@ public final class CaseGroupEndpoint implements CTPEndpoint {
 
   @Autowired
   private CaseGroupService caseGroupService;
+
+  @Autowired
+  private StateTransitionManager<CaseGroupStatus, CategoryDTO.CategoryName> caseGroupStatusTransitionManager;
 
   @Qualifier("caseSvcBeanMapper")
   @Autowired
@@ -54,4 +59,59 @@ public final class CaseGroupEndpoint implements CTPEndpoint {
     return mapperFacade.map(caseGroupObj, CaseGroupDTO.class);
   }
 
+    /**
+     * the GET endpoint to find Case Group transitions by collectionExerciseId and ruRef
+     *
+     * @param collectionExerciseId UUID to find by
+     * @param ruRef String to find by
+     * @return the transitions available and the events needed to put it in this state for the caseGroupId
+     * @throws CTPException something went wrong
+     */
+    @RequestMapping(value = "/transitions/{collectionExerciseId}/{ruRef}", method = RequestMethod.GET)
+    public Map<CategoryDTO.CategoryName, CaseGroupStatus> findTransitionsByCollectionExerciseIdAndRuRef(
+        @PathVariable("collectionExerciseId") final UUID collectionExerciseId,
+        @PathVariable("ruRef") final String ruRef)
+        throws CTPException {
+          log.info("Entering findTransitionsByCollexIdAndRuRef with collectionExerciseId {}, ruRef {}",
+              collectionExerciseId, ruRef);
+
+          CaseGroup caseGroupObj = caseGroupService.findCaseGroupByCollectionExerciseIdAndRuRef(collectionExerciseId, ruRef);
+          if (caseGroupObj == null) {
+              throw new CTPException(CTPException.Fault.RESOURCE_NOT_FOUND, String.format("%s collectionExerciseId %s, ruRef %s",
+                      ERRORMSG_CASEGROUPNOTFOUND, collectionExerciseId, ruRef));
+          }
+          CaseGroupStatus caseGroupStatus = caseGroupObj.getStatus();
+
+          return caseGroupStatusTransitionManager.getAvailableTransitions(caseGroupStatus);
+    }
+
+  /**
+   * the PUT endpoint to change Case Group status from a given Case Group event
+   *
+   * @param collectionExerciseId UUID to find by
+   * @param ruRef String to find by
+   * @param caseGroupEvent Name of event
+   * @return 200 if all is ok, 400 for bad request
+   * @throws CTPException something went wrong
+   */
+  @RequestMapping(value = "/transitions/{collectionExerciseId}/{ruRef}", method = RequestMethod.PUT)
+  public ResponseEntity<?> changeCaseGroupStatus(
+      @PathVariable("collectionExerciseId") final UUID collectionExerciseId,
+      @PathVariable("ruRef") final String ruRef,
+      @RequestBody CaseGroupEvent caseGroupEvent)
+      throws CTPException {
+        log.info("Entering changeCaseGroupStatus with collectionExerciseId {}, ruRef {}, caseGroupEvent {}",
+            collectionExerciseId, ruRef, caseGroupEvent.getEvent());
+
+        CaseGroup caseGroupObj = caseGroupService.findCaseGroupByCollectionExerciseIdAndRuRef(collectionExerciseId, ruRef);
+        if (caseGroupObj == null) {
+          throw new CTPException(CTPException.Fault.RESOURCE_NOT_FOUND, String.format("%s collectionExerciseId %s, ruRef %s",
+              ERRORMSG_CASEGROUPNOTFOUND, collectionExerciseId, ruRef));
+        }
+
+        caseGroupService.transitionCaseGroupStatus(caseGroupObj, CategoryDTO.CategoryName.valueOf(caseGroupEvent.getEvent()),
+            caseGroupObj.getPartyId());
+
+        return ResponseEntity.ok().build();
+  }
 }
--- src/test/java/uk/gov/ons/ctp/response/casesvc/endpoint/CaseGroupEndpointUnitTest.java
+++ src/test/java/uk/gov/ons/ctp/response/casesvc/endpoint/CaseGroupEndpointUnitTest.java
@@ -1,8 +1,9 @@
 package uk.gov.ons.ctp.response.casesvc.endpoint;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ma.glasnost.orika.MapperFacade;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -16,20 +17,28 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.common.error.RestExceptionHandler;
 import uk.gov.ons.ctp.common.jackson.CustomObjectMapper;
+import uk.gov.ons.ctp.common.state.StateTransitionManager;
 import uk.gov.ons.ctp.response.casesvc.CaseSvcBeanMapper;
 import uk.gov.ons.ctp.response.casesvc.domain.model.CaseGroup;
+import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupDTO;
 import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupStatus;
+import uk.gov.ons.ctp.response.casesvc.representation.CategoryDTO;
 import uk.gov.ons.ctp.response.casesvc.service.CaseGroupService;
+import uk.gov.ons.ctp.response.casesvc.state.CaseSvcStateTransitionManagerFactory;
 
+import java.util.Map;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.Is.isA;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.contains;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static uk.gov.ons.ctp.common.MvcHelper.getJson;
+import static uk.gov.ons.ctp.common.MvcHelper.putJson;
 import static uk.gov.ons.ctp.common.utility.MockMvcControllerAdviceHelper.mockAdviceFor;
 
 /**
@@ -46,23 +55,29 @@ public final class CaseGroupEndpointUnitTest {
   @Spy
   private MapperFacade mapperFacade = new CaseSvcBeanMapper();
 
+  @Spy
+  @SuppressWarnings("unchecked")
+  private StateTransitionManager<CaseGroupStatus, CategoryDTO.CategoryName> stm = (StateTransitionManager<CaseGroupStatus, CategoryDTO.CategoryName>)
+      new CaseSvcStateTransitionManagerFactory().getStateTransitionManager(CaseSvcStateTransitionManagerFactory.CASE_GROUP);
+
   private MockMvc mockMvc;
 
   private static final UUID CASE_GROUP_UUID = UUID.randomUUID();
   private static final String NON_EXISTENT_CASE_GROUP_UUID =
-          "9a5f2be5-f944-41f9-982c-3517cfcfe666";
+      "9a5f2be5-f944-41f9-982c-3517cfcfe666";
   private static final UUID CASE_GROUP_UUID_UNCHECKED_EXCEPTION =
-          UUID.randomUUID();
+      UUID.randomUUID();
   private static final UUID CASE_GROUP_CE_ID =
-          UUID.fromString("dab9db7f-3aa0-4866-be20-54d72ee185fb");
+      UUID.fromString("dab9db7f-3aa0-4866-be20-54d72ee185fb");
   private static final UUID CASE_GROUP_PARTY_ID =
-          UUID.fromString("3b136c4b-7a14-4904-9e01-13364dd7b972");
+      UUID.fromString("3b136c4b-7a14-4904-9e01-13364dd7b972");
   private static final String CASE_GROUP_SU_REF = "0123456789";
   private static final String CASE_GROUP_SU_TYPE = "B";
   private static final String OUR_EXCEPTION_MESSAGE = "this is what we throw";
 
   /**
    * Initialises Mockito
+   *
    * @throws Exception exception thrown
    */
   @Before
@@ -70,90 +85,199 @@ public final class CaseGroupEndpointUnitTest {
     MockitoAnnotations.initMocks(this);
 
     this.mockMvc = MockMvcBuilders
-            .standaloneSetup(caseGroupEndpoint)
-            .setHandlerExceptionResolvers(mockAdviceFor(
-                    RestExceptionHandler.class))
-            .setMessageConverters(new MappingJackson2HttpMessageConverter(
-                    new CustomObjectMapper()))
-            .build();
+        .standaloneSetup(caseGroupEndpoint)
+        .setHandlerExceptionResolvers(mockAdviceFor(
+            RestExceptionHandler.class))
+        .setMessageConverters(new MappingJackson2HttpMessageConverter(
+            new CustomObjectMapper()))
+        .build();
   }
 
   /**
    * Tests whether casegroup is found by id
+   *
    * @throws Exception exception thrown
    */
   @Test
   public void findCaseGroupById() throws Exception {
     CaseGroup result = CaseGroup.builder().id(CASE_GROUP_UUID)
-            .collectionExerciseId(CASE_GROUP_CE_ID)
-            .partyId(CASE_GROUP_PARTY_ID)
-            .sampleUnitRef(CASE_GROUP_SU_REF)
-            .sampleUnitType(CASE_GROUP_SU_TYPE).status(CaseGroupStatus.COMPLETE).build();
+        .collectionExerciseId(CASE_GROUP_CE_ID)
+        .partyId(CASE_GROUP_PARTY_ID)
+        .sampleUnitRef(CASE_GROUP_SU_REF)
+        .sampleUnitType(CASE_GROUP_SU_TYPE).status(CaseGroupStatus.COMPLETE).build();
     when(caseGroupService.findCaseGroupById(CASE_GROUP_UUID)).
-            thenReturn(result);
+        thenReturn(result);
 
     ResultActions actions = mockMvc.perform(getJson(
-            String.format("/casegroups/%s", CASE_GROUP_UUID)));
+        String.format("/casegroups/%s", CASE_GROUP_UUID)));
 
     actions.andExpect(status().isOk());
     actions.andExpect(handler().handlerType(CaseGroupEndpoint.class));
     actions.andExpect(handler().methodName("findCaseGroupById"));
     actions.andExpect(jsonPath("$.id",
-            is(CASE_GROUP_UUID.toString())));
+        is(CASE_GROUP_UUID.toString())));
     actions.andExpect(jsonPath("$.collectionExerciseId",
-            is(CASE_GROUP_CE_ID.toString())));
+        is(CASE_GROUP_CE_ID.toString())));
     actions.andExpect(jsonPath("$.partyId",
-            is(CASE_GROUP_PARTY_ID.toString())));
+        is(CASE_GROUP_PARTY_ID.toString())));
     actions.andExpect(jsonPath("$.sampleUnitRef",
-            is(CASE_GROUP_SU_REF)));
+        is(CASE_GROUP_SU_REF)));
     actions.andExpect(jsonPath("$.sampleUnitType",
-            is(CASE_GROUP_SU_TYPE)));
+        is(CASE_GROUP_SU_TYPE)));
     actions.andExpect(jsonPath("$.caseGroupStatus",
-            is(CaseGroupStatus.COMPLETE.toString())));
+        is(CaseGroupStatus.COMPLETE.toString())));
   }
 
   /**
    * Tests whether casegroup is not found by id
+   *
    * @throws Exception exception thrown
    */
   @Test
   public void findCaseGroupByIdNotFound() throws Exception {
     ResultActions actions = mockMvc.perform(getJson(
-            String.format("/casegroups/%s", NON_EXISTENT_CASE_GROUP_UUID)));
+        String.format("/casegroups/%s", NON_EXISTENT_CASE_GROUP_UUID)));
 
     actions.andExpect(status().isNotFound());
     actions.andExpect(handler().handlerType(CaseGroupEndpoint.class));
     actions.andExpect(handler().methodName("findCaseGroupById"));
     actions.andExpect(jsonPath("$.error.code",
-            is(CTPException.Fault.RESOURCE_NOT_FOUND.name())));
+        is(CTPException.Fault.RESOURCE_NOT_FOUND.name())));
     actions.andExpect(jsonPath("$.error.message",
-            is(String.format("CaseGroup not found for casegroup id %s",
-                    NON_EXISTENT_CASE_GROUP_UUID))));
+        is(String.format("CaseGroup not found for casegroup id %s",
+            NON_EXISTENT_CASE_GROUP_UUID))));
     actions.andExpect(jsonPath("$.error.timestamp",
-            isA(String.class)));
+        isA(String.class)));
   }
 
   /**
    * Tests whether casegroup is found by id with an unchecked exception
+   *
    * @throws Exception exception thrown
    */
   @Test
   public void findCaseGroupByIdUnCheckedException() throws Exception {
     when(caseGroupService.
-            findCaseGroupById(CASE_GROUP_UUID_UNCHECKED_EXCEPTION)).
-            thenThrow(new IllegalArgumentException(OUR_EXCEPTION_MESSAGE));
+        findCaseGroupById(CASE_GROUP_UUID_UNCHECKED_EXCEPTION)).
+        thenThrow(new IllegalArgumentException(OUR_EXCEPTION_MESSAGE));
 
     ResultActions actions = mockMvc.perform(getJson(
-            String.format("/casegroups/%s", CASE_GROUP_UUID_UNCHECKED_EXCEPTION)));
+        String.format("/casegroups/%s", CASE_GROUP_UUID_UNCHECKED_EXCEPTION)));
 
     actions.andExpect(status().is5xxServerError());
     actions.andExpect(handler().handlerType(CaseGroupEndpoint.class));
     actions.andExpect(handler().methodName("findCaseGroupById"));
     actions.andExpect(jsonPath("$.error.code",
-            is(CTPException.Fault.SYSTEM_ERROR.name())));
+        is(CTPException.Fault.SYSTEM_ERROR.name())));
     actions.andExpect(jsonPath("$.error.message",
-            is(OUR_EXCEPTION_MESSAGE)));
+        is(OUR_EXCEPTION_MESSAGE)));
     actions.andExpect(jsonPath("$.error.timestamp",
-            isA(String.class)));
+        isA(String.class)));
+  }
+
+
+  /**
+   * Test available case group transitions are found by collection exercise Id and RU ref
+   *
+   * @throws Exception exception thrown
+   */
+  @Test
+  public void givenCollexIdAndRuRefWhenRequestForCaseGroupTransitionsThenAvailableCaseGroupTransitionsReturned() throws Exception {
+    // Given
+    CaseGroup result = CaseGroup.builder()
+        .id(CASE_GROUP_UUID)
+        .collectionExerciseId(CASE_GROUP_CE_ID)
+        .partyId(CASE_GROUP_PARTY_ID)
+        .sampleUnitRef(CASE_GROUP_SU_REF)
+        .sampleUnitType(CASE_GROUP_SU_TYPE)
+        .status(CaseGroupStatus.NOTSTARTED).build();
+
+    when(caseGroupService.findCaseGroupByCollectionExerciseIdAndRuRef(CASE_GROUP_CE_ID, CASE_GROUP_SU_REF)).thenReturn(result);
+
+    // When
+    ResultActions actions = mockMvc.perform(getJson(
+        String.format("/casegroups/transitions/%s/%s", CASE_GROUP_CE_ID, CASE_GROUP_SU_REF)));
+
+    // Then
+    actions.andExpect(status().isOk());
+
+    String availableTransitionsString = actions.andReturn().getResponse().getContentAsString();
+    Map<CategoryDTO.CategoryName, CaseGroupStatus> availableTransitions = new ObjectMapper()
+        .readValue(availableTransitionsString, new TypeReference<Map<CategoryDTO.CategoryName, CaseGroupStatus>>() {});
+    assertThat(availableTransitions, hasEntry(CategoryDTO.CategoryName.COLLECTION_INSTRUMENT_DOWNLOADED, CaseGroupStatus.INPROGRESS));
+    assertThat(availableTransitions, hasEntry(CategoryDTO.CategoryName.SUCCESSFUL_RESPONSE_UPLOAD, CaseGroupStatus.COMPLETE));
+    assertThat(availableTransitions, hasEntry(CategoryDTO.CategoryName.COMPLETED_BY_PHONE, CaseGroupStatus.COMPLETEDBYPHONE));
+  }
+
+  /**
+   * Test available case group transitions not found
+   *
+   * @throws Exception exception thrown
+   */
+  @Test
+  public void givenInvalidCollexIdAndRuRefWhenRequestForCaseGroupTransitionsThenCaseGroupNotFound() throws Exception {
+    // Given
+    String nonExistentCollexId = UUID.randomUUID().toString();
+    String nonExistentRuRef = "11111111111";
+
+    // When
+    ResultActions actions = mockMvc.perform(getJson(
+        String.format("/casegroups/transitions/%s/%s", nonExistentCollexId, nonExistentRuRef)));
+
+    // Then
+    actions.andExpect(status().isNotFound());
+   }
+
+  /**
+   * Test case group status transition is successful
+   *
+   * @throws Exception exception thrown
+   */
+  @Test
+  public void givenCollexIdRuRefAndCaseEventWhenChangeCaseGroupStatusThenSuccess() throws Exception {
+    // Given
+    CaseGroup caseGroupResult = CaseGroup.builder()
+        .id(CASE_GROUP_UUID)
+        .collectionExerciseId(CASE_GROUP_CE_ID)
+        .partyId(CASE_GROUP_PARTY_ID)
+        .sampleUnitRef(CASE_GROUP_SU_REF)
+        .sampleUnitType(CASE_GROUP_SU_TYPE)
+        .status(CaseGroupStatus.NOTSTARTED).build();
+    when(caseGroupService.findCaseGroupByCollectionExerciseIdAndRuRef(CASE_GROUP_CE_ID, CASE_GROUP_SU_REF)).thenReturn(caseGroupResult);
+
+    CategoryDTO.CategoryName categoryName = CategoryDTO.CategoryName.COMPLETED_BY_PHONE;
+
+    // When
+    CaseGroupEvent caseGroupEvent = new CaseGroupEvent();
+    caseGroupEvent.setEvent("COMPLETED_BY_PHONE");
+    ResultActions actions = mockMvc.perform(putJson(
+        String.format("/casegroups/transitions/%s/%s", CASE_GROUP_CE_ID,
+            CASE_GROUP_SU_REF), new ObjectMapper().writeValueAsString(caseGroupEvent)));
+
+    // Then
+    actions.andExpect(status().isOk());
+    verify(caseGroupService).transitionCaseGroupStatus(caseGroupResult, categoryName, CASE_GROUP_PARTY_ID);
+  }
+
+  /**
+   * Test case group status transition not found
+   *
+   * @throws Exception exception thrown
+   */
+  @Test
+  public void givenInvalidCollexIdPartyIdAndRuRefWhenPutCaseGroupTransitionThenCaseGroupTransitionNotFound() throws Exception {
+    // Given
+    String nonExistentCollexId = UUID.randomUUID().toString();
+    String nonExistentPartyId = UUID.randomUUID().toString();
+    String nonExistentRuRef = "11111111111";
+    CategoryDTO.CategoryName categoryName = CategoryDTO.CategoryName.COMPLETED_BY_PHONE;
+
+    // When
+    ResultActions actions = mockMvc.perform(putJson(
+        String.format("/casegroups/transitions/%s/%s/%s/%s", nonExistentCollexId, nonExistentPartyId,
+            nonExistentRuRef, categoryName), ""));
+
+    // Then
+    actions.andExpect(status().isNotFound());
   }
 }
