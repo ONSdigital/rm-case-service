9991e330f9fc782c57cb017bfde9df4a2c47a1f1
Matt Innes <matt@madeye.com>
2018-03-13 11:36:13 +0000

    Codacy fixes part deux

--- src/main/java/uk/gov/ons/ctp/response/casesvc/service/impl/CaseGroupServiceImpl.java
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/service/impl/CaseGroupServiceImpl.java
@@ -13,9 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.common.state.StateTransitionManager;
 import uk.gov.ons.ctp.response.casesvc.domain.model.Case;
-import uk.gov.ons.ctp.response.casesvc.domain.model.CaseEvent;
 import uk.gov.ons.ctp.response.casesvc.domain.model.CaseGroup;
-import uk.gov.ons.ctp.response.casesvc.domain.model.Category;
 import uk.gov.ons.ctp.response.casesvc.domain.repository.CaseGroupRepository;
 import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupStatus;
 import uk.gov.ons.ctp.response.casesvc.representation.CategoryDTO;
@@ -94,15 +92,31 @@ public class CaseGroupServiceImpl implements CaseGroupService {
    * Use case group to find collectionEx
    * Then use CollEx service to find survey ID, then find all collexs for survey
    * Then get all case groups for the party ID where Collex ID is in list of collexs
+   *
+   * @param targetCase the case for which related case groups should be found
+   * @return a list of other case groups related to the target by party and collection exercise
+   * @throws CTPException thrown if database error etc
    */
   public List<CaseGroup> transitionOtherCaseGroups(final Case targetCase) throws CTPException {
-    // TODO: null checking
+    if (targetCase == null){
+        throw new CTPException(CTPException.Fault.SYSTEM_ERROR, "Target case must be supplied");
+    }
     CaseGroup caseGroup = caseGroupRepo.findOne(targetCase.getCaseGroupFK());
+    if (caseGroup == null){
+        throw new CTPException(CTPException.Fault.RESOURCE_NOT_FOUND, String.format("Cannot find case group %s", targetCase.getCaseGroupFK()));
+    }
     CollectionExerciseDTO collectionExercise = collectionExerciseSvcClientService
             .getCollectionExercise(caseGroup.getCollectionExerciseId());
+    if (collectionExercise == null){
+      throw new CTPException(CTPException.Fault.RESOURCE_NOT_FOUND, String.format("Cannot find collection exercise %s", caseGroup.getCollectionExerciseId()));
+    }
     // fetch all the collection exercises for a survey
     List<CollectionExerciseDTO> collectionExercises =
             collectionExerciseSvcClientService.getCollectionExercises(collectionExercise.getSurveyId());
+    if (collectionExercises == null){
+      // This will happen if say the collection exercise service is down
+      throw new CTPException(CTPException.Fault.SYSTEM_ERROR, String.format("Cannot find collection exercises for survey %s", collectionExercise.getSurveyId());
+    }
     // get published collection exercise
     List<CollectionExerciseDTO> publishedCollexs =
             collectionExercises
