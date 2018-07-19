4dc50e5400b15f6e0353a641c318a5140d8711f9
Emilio Ward <emilioward@hotmail.co.uk>
2018-07-19 09:33:28 +0100

    updated sql scripts with additional case categories

--- src/main/resources/database/changes/release-10.59.0/add-H-type-to-case-categories.sql
+++ src/main/resources/database/changes/release-10.59.0/add-H-type-to-case-categories.sql
@@ -1,6 +1,19 @@
 UPDATE casesvc.category
-SET shortdescription='Action Created', longdescription='Action Created', eventtype=NULL, "role"=NULL, generatedactiontype=NULL, "group"=NULL, oldcasesampleunittypes='B,BI,H', newcasesampleunittype=NULL, recalccollectioninstrument=NULL
-WHERE categorypk='ACTION_CREATED' OR categorypk='ACTION_UPDATED' OR categorypk='ACTION_COMPLETED' OR categorypk='CASE_CREATED';
-UPDATE casesvc.category
-SET shortdescription='Action Created', longdescription='Action Created', eventtype=NULL, "role"=NULL, generatedactiontype=NULL, "group"=NULL, oldcasesampleunittypes='BI,H', newcasesampleunittype=NULL, recalccollectioninstrument=NULL
+SET oldcasesampleunittypes='BI,H'
 WHERE categorypk='SUCCESSFUL_RESPONSE_UPLOAD';
+
+UPDATE casesvc.category
+SET oldcasesampleunittypes='B,H'
+WHERE categorypk='ACCESS_CODE_AUTHENTICATION_ATTEMPT';
+
+UPDATE casesvc.category
+SET oldcasesampleunittypes='B,BI,H'
+WHERE categorypk='ACTION_CANCELLATION_CREATED'
+OR categorypk='COMPLETED_BY_PHONE'
+OR categorypk='NO_LONGER_REQUIRED'
+OR categorypk='ACTION_CANCELLATION_COMPLETED'
+OR categorypk='EQ_LAUNCH'
+OR categorypk='CASE_CREATED'
+OR categorypk='ACTION_COMPLETED'
+OR categorypk='ACTION_UPDATED'
+OR categorypk='ACTION_CREATED';
