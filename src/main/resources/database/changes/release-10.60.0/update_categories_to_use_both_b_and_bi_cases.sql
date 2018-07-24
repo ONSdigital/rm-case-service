aa2d50f54c1f2502ce76b609100350f1f3bfe70d
gemmai95 <gemma.i_95@hotmail.co.uk>
2018-07-24 09:20:37 +0100

    Update update_categories_to_use_both_b_and_bi_cases.sql

--- src/main/resources/database/changes/release-10.60.0/update_categories_to_use_both_b_and_bi_cases.sql
+++ src/main/resources/database/changes/release-10.60.0/update_categories_to_use_both_b_and_bi_cases.sql
@@ -1,4 +1,4 @@
 UPDATE casesvc.category
-SET oldcasesampleunittypes = 'B,BI'
+SET oldcasesampleunittypes = 'B,BI,H'
 WHERE categorypk IN ('COLLECTION_INSTRUMENT_DOWNLOADED', 'COLLECTION_INSTRUMENT_ERROR',
 'UNSUCCESSFUL_RESPONSE_UPLOAD', 'SUCCESSFUL_RESPONSE_UPLOAD');
