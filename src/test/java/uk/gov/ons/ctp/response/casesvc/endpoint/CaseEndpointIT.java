0d3aa665b779291f03d6b8b7ce2f389df0405e76
Ben Jefferies <benjefferies@echosoft.uk>
2018-08-23 09:09:39 +0100

    Add integration test

--- src/test/java/uk/gov/ons/ctp/response/casesvc/endpoint/CaseEndpointIT.java
+++ src/test/java/uk/gov/ons/ctp/response/casesvc/endpoint/CaseEndpointIT.java
@@ -1,11 +1,7 @@
 package uk.gov.ons.ctp.response.casesvc.endpoint;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.junit.Assert.assertNotNull;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
@@ -13,57 +9,40 @@ import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import javax.xml.bind.JAXBContext;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.gov.ons.ctp.common.UnirestInitialiser;
-import uk.gov.ons.ctp.common.utility.Mapzer;
-import uk.gov.ons.ctp.response.casesvc.config.AppConfig;
+import uk.gov.ons.ctp.response.casesvc.CaseCreator;
 import uk.gov.ons.ctp.response.casesvc.message.notification.CaseNotification;
-import uk.gov.ons.ctp.response.casesvc.message.sampleunitnotification.SampleUnitParent;
 import uk.gov.ons.ctp.response.casesvc.representation.CaseDetailsDTO;
 import uk.gov.ons.ctp.response.casesvc.representation.CaseEventCreationRequestDTO;
 import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupStatus;
 import uk.gov.ons.ctp.response.casesvc.representation.CategoryDTO.CategoryName;
 import uk.gov.ons.ctp.response.casesvc.representation.CreatedCaseEventDTO;
-import uk.gov.ons.tools.rabbit.Rabbitmq;
-import uk.gov.ons.tools.rabbit.SimpleMessageBase;
-import uk.gov.ons.tools.rabbit.SimpleMessageListener;
-import uk.gov.ons.tools.rabbit.SimpleMessageSender;
 
 @ContextConfiguration
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class CaseEndpointIT {
   private static final Logger log = LoggerFactory.getLogger(CaseEndpointIT.class);
 
-  @ClassRule public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-  @Rule public final SpringMethodRule springMethodRule = new SpringMethodRule();
-
   @Rule public WireMockRule wireMockRule = new WireMockRule(options().port(18002));
-
-  @Autowired private ResourceLoader resourceLoader;
 
   @LocalServerPort private int port;
 
-  @Autowired private ObjectMapper mapper;
-
-  @Autowired private AppConfig appConfig;
+  @Autowired private CaseCreator caseCreator;
 
   @BeforeClass
   public static void setUp() {
@@ -75,7 +54,7 @@ public class CaseEndpointIT {
   public void ensureSampleUnitIdReceived() throws Exception {
     UUID sampleUnitId = UUID.randomUUID();
 
-    CaseNotification caseNotification = sendSampleUnit("LMS0001", "H", sampleUnitId);
+    CaseNotification caseNotification = caseCreator.sendSampleUnit("LMS0001", "H", sampleUnitId);
 
     assertThat(caseNotification.getSampleUnitId()).isEqualTo(sampleUnitId.toString());
   }
@@ -84,7 +63,8 @@ public class CaseEndpointIT {
   public void testCreateSocialCaseEvents() throws Exception {
 
     // Given
-    CaseNotification caseNotification = sendSampleUnit("LMS0002", "H", UUID.randomUUID());
+    CaseNotification caseNotification =
+        caseCreator.sendSampleUnit("LMS0002", "H", UUID.randomUUID());
 
     String caseID = caseNotification.getCaseId();
     CaseEventCreationRequestDTO caseEventCreationRequestDTO =
@@ -107,7 +87,7 @@ public class CaseEndpointIT {
   public void ensureCaseReturnedBySampleUnitId() throws Exception {
 
     UUID sampleUnitId = UUID.randomUUID();
-    CaseNotification caseNotif = sendSampleUnit("LMS0003", "H", sampleUnitId);
+    CaseNotification caseNotif = caseCreator.sendSampleUnit("LMS0003", "H", sampleUnitId);
 
     UUID caseId = UUID.fromString(caseNotif.getCaseId());
 
@@ -126,14 +106,13 @@ public class CaseEndpointIT {
   /**
    * Test Collection Instrument downloaded case event works with B cases, and case group status has
    * transitioned to InProgress.
-   *
-   * @throws Exception
    */
   @Test
   public void testCreateCollectionInstrumentDownloadedCaseEventWithBCaseSuccess() throws Exception {
 
     // Given
-    CaseNotification caseNotification = sendSampleUnit("BS12345", "B", UUID.randomUUID());
+    CaseNotification caseNotification =
+        caseCreator.sendSampleUnit("BS12345", "B", UUID.randomUUID());
 
     String caseID = caseNotification.getCaseId();
     CaseEventCreationRequestDTO caseEventCreationRequestDTO =
@@ -168,14 +147,13 @@ public class CaseEndpointIT {
   /**
    * Test Collection Instrument downloaded case event works with B cases, and case group status has
    * not transitioned to InProgress.
-   *
-   * @throws Exception
    */
   @Test
   public void testCreateCollectionInstrumentErrorCaseEventWithBCaseSuccess() throws Exception {
 
     // Given
-    CaseNotification caseNotification = sendSampleUnit("BS12345", "B", UUID.randomUUID());
+    CaseNotification caseNotification =
+        caseCreator.sendSampleUnit("BS12345", "B", UUID.randomUUID());
 
     String caseID = caseNotification.getCaseId();
     CaseEventCreationRequestDTO caseEventCreationRequestDTO =
@@ -210,14 +188,13 @@ public class CaseEndpointIT {
   /**
    * Test Successful response upload case event works with B cases, and case group status has
    * transitioned to Complete.
-   *
-   * @throws Exception
    */
   @Test
   public void testCreateSuccessfulResponseUploadCaseEventWithBCaseSuccess() throws Exception {
 
     // Given
-    CaseNotification caseNotification = sendSampleUnit("BS12345", "B", UUID.randomUUID());
+    CaseNotification caseNotification =
+        caseCreator.sendSampleUnit("BS12345", "B", UUID.randomUUID());
 
     String caseID = caseNotification.getCaseId();
     CaseEventCreationRequestDTO caseEventCreationRequestDTO =
@@ -252,14 +229,13 @@ public class CaseEndpointIT {
   /**
    * Test Unsuccessful Response Upload case event works with B cases, and case group status has not
    * transitioned to Complete.
-   *
-   * @throws Exception
    */
   @Test
   public void testCreateUnsuccessfulResponseUploadCaseEventWithBCaseSuccess() throws Exception {
 
     // Given
-    CaseNotification caseNotification = sendSampleUnit("BS12345", "B", UUID.randomUUID());
+    CaseNotification caseNotification =
+        caseCreator.sendSampleUnit("BS12345", "B", UUID.randomUUID());
 
     String caseID = caseNotification.getCaseId();
     CaseEventCreationRequestDTO caseEventCreationRequestDTO =
@@ -289,90 +265,5 @@ public class CaseEndpointIT {
     assertThat(createdCaseResponse.getStatus()).isEqualTo(201);
     assertThat(affectedCase.getCaseGroup().getCaseGroupStatus())
         .isNotEqualTo(CaseGroupStatus.COMPLETE);
-  }
-
-  /**
-   * Creates a new SimpleMessageSender based on the config in AppConfig
-   *
-   * @return a new SimpleMessageSender
-   */
-  private SimpleMessageSender getMessageSender() {
-    Rabbitmq config = this.appConfig.getRabbitmq();
-
-    return new SimpleMessageSender(
-        config.getHost(), config.getPort(), config.getUsername(), config.getPassword());
-  }
-
-  /**
-   * Creates a new SimpleMessageListener based on the config in AppConfig
-   *
-   * @return a new SimpleMessageListener
-   */
-  private SimpleMessageListener getMessageListener() {
-    Rabbitmq config = this.appConfig.getRabbitmq();
-
-    return new SimpleMessageListener(
-        config.getHost(), config.getPort(), config.getUsername(), config.getPassword());
-  }
-
-  private void createIACStub() throws IOException {
-    this.wireMockRule.stubFor(
-        post(urlPathEqualTo("/iacs"))
-            .willReturn(
-                aResponse()
-                    .withHeader("Content-Type", "application/json")
-                    .withBody("[\"grtt7x2nhygg\"]")));
-  }
-
-  /**
-   * Sends a sample unit in a message so that casesvc creates a case, then waits for a message on
-   * the case lifecycle queue which confirms case creation
-   *
-   * @return a new CaseNotification
-   */
-  private CaseNotification sendSampleUnit(
-      String sampleUnitRef, String sampleUnitType, UUID sampleUnitId) throws Exception {
-    createIACStub();
-
-    SimpleMessageSender sender = getMessageSender();
-
-    SampleUnitParent sampleUnit = new SampleUnitParent();
-    sampleUnit.setCollectionExerciseId(UUID.randomUUID().toString());
-    sampleUnit.setId(sampleUnitId.toString());
-    sampleUnit.setActionPlanId(UUID.randomUUID().toString());
-    sampleUnit.setSampleUnitRef(sampleUnitRef);
-    sampleUnit.setCollectionInstrumentId(UUID.randomUUID().toString());
-    sampleUnit.setPartyId(UUID.randomUUID().toString());
-    sampleUnit.setSampleUnitType(sampleUnitType);
-
-    JAXBContext jaxbContext = JAXBContext.newInstance(SampleUnitParent.class);
-    String xml =
-        new Mapzer(resourceLoader)
-            .convertObjectToXml(
-                jaxbContext, sampleUnit, "casesvc/xsd/inbound/SampleUnitNotification.xsd");
-
-    sender.sendMessage("collection-inbound-exchange", "Case.CaseDelivery.binding", xml);
-
-    String message = waitForNotification();
-
-    jaxbContext = JAXBContext.newInstance(CaseNotification.class);
-    return (CaseNotification)
-        jaxbContext.createUnmarshaller().unmarshal(new ByteArrayInputStream(message.getBytes()));
-  }
-
-  private String waitForNotification() throws Exception {
-
-    SimpleMessageListener listener = getMessageListener();
-    BlockingQueue<String> queue =
-        listener.listen(
-            SimpleMessageBase.ExchangeType.Direct,
-            "case-outbound-exchange",
-            "Case.LifecycleEvents.binding");
-
-    String message = queue.take();
-    log.info("message = " + message);
-    assertNotNull("Timeout waiting for message to arrive in Case.LifecycleEvents", message);
-
-    return message;
   }
 }
