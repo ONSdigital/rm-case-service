5c8bc7a328e63ccbc8c4ae7bdc7a811d152703c1
Nick Grant <nick.grant@ext.ons.gov.uk>
2018-09-12 11:44:25 +0100

    Roll back most of the Rabbit changes. Fix the tests.

--- src/test/java/uk/gov/ons/ctp/response/casesvc/message/RabbitSpringIntegrationDLQAndRetriesIT.java
+++ src/test/java/uk/gov/ons/ctp/response/casesvc/message/RabbitSpringIntegrationDLQAndRetriesIT.java
@@ -1,47 +1,27 @@
 package uk.gov.ons.ctp.response.casesvc.message;
 
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
-import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import java.io.ByteArrayInputStream;
-import java.sql.Time;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.datatype.XMLGregorianCalendar;
-import org.aopalliance.aop.Advice;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.integration.channel.QueueChannel;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.gov.ons.ctp.common.time.DateTimeUtil;
 import uk.gov.ons.ctp.common.utility.Mapzer;
-import uk.gov.ons.ctp.response.casesvc.CaseCreator;
 import uk.gov.ons.ctp.response.casesvc.config.AppConfig;
 import uk.gov.ons.ctp.response.casesvc.message.feedback.CaseReceipt;
 import uk.gov.ons.ctp.response.casesvc.message.feedback.InboundChannel;
 import uk.gov.ons.ctp.response.casesvc.message.sampleunitnotification.SampleUnitParent;
-import uk.gov.ons.ctp.response.casesvc.service.CaseService;
 import uk.gov.ons.tools.rabbit.SimpleMessageBase.ExchangeType;
 import uk.gov.ons.tools.rabbit.SimpleMessageListener;
 import uk.gov.ons.tools.rabbit.SimpleMessageSender;
@@ -54,35 +34,28 @@ public class RabbitSpringIntegrationDLQAndRetriesIT {
 
   @LocalServerPort private int port;
 
-  @Autowired
-  AppConfig appConfig;
+  @Autowired AppConfig appConfig;
 
   @Autowired private ResourceLoader resourceLoader;
-
-  @Rule
-  public WireMockRule wireMockRule =
-      new WireMockRule(options().extensions(new ResponseTemplateTransformer(false)).port(18002));
-
-  @MockBean private CaseService caseService;
 
   private SimpleMessageListener listener;
   private SimpleMessageSender sender;
 
   @Before
   public void setup() {
-    sender = new SimpleMessageSender(
+    sender =
+        new SimpleMessageSender(
             appConfig.getRabbitmq().getHost(), appConfig.getRabbitmq().getPort(),
             appConfig.getRabbitmq().getUsername(), appConfig.getRabbitmq().getPassword());
 
-    listener = new SimpleMessageListener(
+    listener =
+        new SimpleMessageListener(
             appConfig.getRabbitmq().getHost(), appConfig.getRabbitmq().getPort(),
             appConfig.getRabbitmq().getUsername(), appConfig.getRabbitmq().getPassword());
   }
 
   @Test
-  public void ensureFiniteRetriesOnFailedCaseNotification() throws Exception {
-    doThrow(new RuntimeException()).when(caseService).createInitialCase(any());
-
+  public void ensureFailedCaseNotificationIsDeadLetterQueuedAfterFiniteRetries() throws Exception {
     UUID sampleUnitId = UUID.randomUUID();
     SampleUnitParent sampleUnit = new SampleUnitParent();
     sampleUnit.setCollectionExerciseId(UUID.randomUUID().toString());
@@ -90,7 +63,7 @@ public class RabbitSpringIntegrationDLQAndRetriesIT {
     sampleUnit.setActionPlanId(UUID.randomUUID().toString());
     sampleUnit.setSampleUnitRef("LMS0004");
     sampleUnit.setCollectionInstrumentId(UUID.randomUUID().toString());
-    sampleUnit.setSampleUnitType("H");
+    sampleUnit.setSampleUnitType("VALUE_WHICH_WILL_SOME_SORT_OF_EXCEPTION_BECAUSE_ITS_GARBAGE");
 
     JAXBContext jaxbContext = JAXBContext.newInstance(SampleUnitParent.class);
     String xml =
@@ -98,49 +71,47 @@ public class RabbitSpringIntegrationDLQAndRetriesIT {
             .convertObjectToXml(
                 jaxbContext, sampleUnit, "casesvc/xsd/inbound/SampleUnitNotification.xsd");
 
-
     sender.sendMessageToQueue("Case.CaseDelivery", xml);
-    String message = listener.listen(ExchangeType.Direct,
-        "case-deadletter-exchange", "Case.CaseDelivery.binding").take();
+    String message =
+        listener
+            .listen(ExchangeType.Direct, "case-deadletter-exchange", "Case.CaseDelivery.binding")
+            .poll(30, TimeUnit.SECONDS);
 
-    SampleUnitParent received = (SampleUnitParent) jaxbContext
-            .createUnmarshaller()
-            .unmarshal(new ByteArrayInputStream(message.getBytes()));
+    SampleUnitParent received =
+        (SampleUnitParent)
+            jaxbContext
+                .createUnmarshaller()
+                .unmarshal(new ByteArrayInputStream(message.getBytes()));
 
     assertEquals(received.getId(), sampleUnit.getId());
-
-    verify(caseService, times(3)).createInitialCase(any());
   }
 
   @Test
-  public void ensureFiniteRetriesOnFailedCaseReceipt() throws Exception {
-    doThrow(new RuntimeException()).when(caseService).findCaseById(any());
-
-    UUID caseId = UUID.randomUUID();
+  public void ensureFailedCaseReceiptIsDeadLetterQueuedAfterFiniteRetries() throws Exception {
+    String caseId = "VALUE_WHICH_WILL_SOME_SORT_OF_EXCEPTION_BECAUSE_ITS_GARBAGE";
     CaseReceipt caseReceipt = new CaseReceipt();
-    caseReceipt.setCaseId(caseId.toString());
+    caseReceipt.setCaseId(caseId);
     caseReceipt.setInboundChannel(InboundChannel.PAPER);
     caseReceipt.setResponseDateTime(DateTimeUtil.giveMeCalendarForNow());
-    caseReceipt.setCaseRef("NOODLE");
+    caseReceipt.setCaseRef("TESTCASEREF");
 
     JAXBContext jaxbContext = JAXBContext.newInstance(CaseReceipt.class);
     String xml =
         new Mapzer(resourceLoader)
-            .convertObjectToXml(
-                jaxbContext, caseReceipt, "casesvc/xsd/inbound/caseReceipt.xsd");
-
+            .convertObjectToXml(jaxbContext, caseReceipt, "casesvc/xsd/inbound/caseReceipt.xsd");
 
     sender.sendMessageToQueue("Case.Responses", xml);
-    String message = listener.listen(ExchangeType.Direct,
-        "case-deadletter-exchange", "Case.Responses.binding").poll(
-        30, TimeUnit.SECONDS);
+    String message =
+        listener
+            .listen(ExchangeType.Direct, "case-deadletter-exchange", "Case.Responses.binding")
+            .poll(30, TimeUnit.SECONDS);
 
-    CaseReceipt dlqCaseReceipt = (CaseReceipt) jaxbContext
-        .createUnmarshaller()
-        .unmarshal(new ByteArrayInputStream(message.getBytes()));
+    CaseReceipt dlqCaseReceipt =
+        (CaseReceipt)
+            jaxbContext
+                .createUnmarshaller()
+                .unmarshal(new ByteArrayInputStream(message.getBytes()));
 
     assertEquals(caseReceipt.getCaseId(), dlqCaseReceipt.getCaseId());
-
-    verify(caseService, times(3)).findCaseById(any());
   }
 }
