1397c86f802ffaad2e632768bc13a1056f8c647c
Warren Bailey <warren@warrenbailey.net>
2020-06-30 10:48:11 +0100

    logmessage needs to be last

--- src/main/resources/database/changes/release-21/drop-function.sql
+++ src/main/resources/database/changes/release-21/drop-function.sql
@@ -1,3 +1,3 @@
-DROP FUNCTION casesvc.generate_case_events_report;
-DROP FUNCTION casesvc.logmessage;
-DROP FUNCTION casesvc.generate_response_chasing_report;
+DROP FUNCTION IF EXISTS casesvc.generate_case_events_report();
+DROP FUNCTION IF EXISTS casesvc.generate_response_chasing_report();
+DROP FUNCTION IF EXISTS casesvc.logmessage(text, numeric, text, text);
