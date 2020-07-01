c5b66fe381e681ff5264650c301785119b86fac8
Adam Mann <manny160389@hotmail.com>
2020-07-01 09:43:27 +0100

    Remove unused /categories/name/<name> endpoint (#144)
    * Remove unused /categories/name/<name> endpoint
    
    * Formatting errors
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    Co-authored-by: ras-rm-bot <65395820+ras-rm-bot@users.noreply.github.com>

--- src/main/java/uk/gov/ons/ctp/response/casesvc/endpoint/CategoryEndpoint.java
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/endpoint/CategoryEndpoint.java
@@ -3,13 +3,11 @@ package uk.gov.ons.ctp.response.casesvc.endpoint;
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
 import java.util.List;
-import java.util.Optional;
 import ma.glasnost.orika.MapperFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +16,6 @@ import uk.gov.ons.ctp.response.casesvc.domain.model.Category;
 import uk.gov.ons.ctp.response.casesvc.representation.CategoryDTO;
 import uk.gov.ons.ctp.response.casesvc.service.CategoryService;
 import uk.gov.ons.ctp.response.lib.common.endpoint.CTPEndpoint;
-import uk.gov.ons.ctp.response.lib.common.error.CTPException;
 
 /** The REST endpoint controller for Category */
 @RestController
@@ -33,32 +30,6 @@ public final class CategoryEndpoint implements CTPEndpoint {
   @Qualifier("caseSvcBeanMapper")
   @Autowired
   private MapperFacade mapperFacade;
-
-  /**
-   * the GET endpoint to retrieve the category with categoryType
-   *
-   * @param categoryName the categoryName
-   * @return the list of categories
-   * @throws CTPException if category does not exist
-   */
-  @RequestMapping(value = "/name/{categoryName}", method = RequestMethod.GET)
-  public CategoryDTO findCategory(@PathVariable("categoryName") final String categoryName)
-      throws CTPException {
-    log.with("category_name", categoryName).debug("Entering findCategory");
-
-    Category category = null;
-    Optional<CategoryDTO.CategoryName> catTypeEnum =
-        CategoryDTO.CategoryName.fromString(categoryName);
-    if (catTypeEnum.isPresent()) {
-      category = categoryService.findCategory(catTypeEnum.get());
-    }
-    if (category == null) {
-      throw new CTPException(
-          CTPException.Fault.RESOURCE_NOT_FOUND,
-          String.format("%s categoryName %s", ERRORMSG_CATEGORYNOTFOUND, categoryName));
-    }
-    return mapperFacade.map(category, CategoryDTO.class);
-  }
 
   /**
    * the GET endpoint to retrieve all categories
