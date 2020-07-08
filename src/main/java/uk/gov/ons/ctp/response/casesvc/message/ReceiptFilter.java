32e9da4809bae5197716728a44c07c804dac1cc0
Jon Kent <jonkent95@gmail.com>
2020-07-08 16:23:23 +0100

    Validation of json payload in ReceiptFilter

--- src/main/java/uk/gov/ons/ctp/response/casesvc/message/ReceiptFilter.java
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/message/ReceiptFilter.java
@@ -1,13 +1,33 @@
 package uk.gov.ons.ctp.response.casesvc.message;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.godaddy.logging.Logger;
+import com.godaddy.logging.LoggerFactory;
 import org.springframework.integration.core.MessageSelector;
 import org.springframework.messaging.Message;
+import uk.gov.ons.ctp.response.casesvc.message.feedback.CaseReceipt;
+
+import java.io.IOException;
 
 public class ReceiptFilter implements MessageSelector {
 
+    private static final Logger log = LoggerFactory.getLogger(ReceiptFilter.class);
+
     @Override
     public boolean accept(Message<?> message) {
-        //how do we validate this?
+        ObjectMapper obj = new ObjectMapper();
+        try{
+            obj.readValue(message.getPayload().toString(), CaseReceipt.class);
+        }
+        catch (JsonParseException jpe){
+            log.error("Failed to parse receipt", jpe);
+            return false;
+        }
+        catch (IOException ioe){
+            log.error("Unexpected exception occurred", ioe);
+            return false;
+        }
         return true;
     }
 }
