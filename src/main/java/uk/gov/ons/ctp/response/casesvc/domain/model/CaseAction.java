dbee52cf980c0196f2da3cf645ab9aa3f06f9358
Amit Sinha <amit@creativeappdevs.com>
2021-09-28 16:18:44 +0100

    Adding case action view to case in prepration of action-case merge

---
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/domain/model/CaseAction.java
@@ -0,0 +1,50 @@
+package uk.gov.ons.ctp.response.casesvc.domain.model;
+
+import java.util.UUID;
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import uk.gov.ons.ctp.response.casesvc.representation.CaseGroupStatus;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CaseAction {
+
+  @Id
+  @Column(name = "collection_exercise_id")
+  private UUID collectionExerciseId;
+
+  @Column(name = "case_id")
+  private UUID caseId;
+
+  @Column(name = "party_id")
+  private UUID partyId;
+
+  @Column(name = "sample_unit_ref")
+  private String sampleUnitRef;
+
+  @Column(name = "sample_unit_type")
+  private String sampleUnitType;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status")
+  private CaseGroupStatus status;
+
+  @Column(name = "survey_id")
+  private UUID surveyId;
+
+  @Column(name = "sample_unit_id")
+  private UUID sampleUnitId;
+
+  @Column(name = "collection_instrument_id")
+  private UUID collectionInstrumentId;
+
+  @Column(name = "iac")
+  private String iac;
+
+  @Column(name = "active_enrolment")
+  private boolean activeEnrolment;
+}
---
+++ src/test/resources/delete-test-data.sql
@@ -0,0 +1,3 @@
+Delete from casesvc.caseevent;
+Delete from casesvc.case;
+Delete from casesvc.casegroup;
