51220fafa8b39d64961be01398ed31002e5de2e5
Matt <matt.crocker@gmail.com>
2021-10-26 14:11:41 +0100

    Remove more unused categories

---
+++ src/main/resources/database/changes/release-30/remove_more_unused_categories.sql
@@ -0,0 +1,11 @@
+DELETE FROM casesvc.caseevent WHERE category_fk = 'LANGUAGE_DIFFICULTIES';
+DELETE FROM casesvc.category WHERE category_pk = 'LANGUAGE_DIFFICULTIES';
+
+DELETE FROM casesvc.caseevent WHERE category_fk = 'TOO_BUSY';
+DELETE FROM casesvc.category WHERE category_pk = 'TOO_BUSY';
+
+DELETE FROM casesvc.caseevent WHERE category_fk = 'NON_RESIDENTIAL_ADDRESS';
+DELETE FROM casesvc.category WHERE category_pk = 'NON_RESIDENTIAL_ADDRESS';
+
+DELETE FROM casesvc.caseevent WHERE category_fk = 'RESPONDENT_EMAIL_AMENDED';
+DELETE FROM casesvc.category WHERE category_pk = 'RESPONDENT_EMAIL_AMENDED';
