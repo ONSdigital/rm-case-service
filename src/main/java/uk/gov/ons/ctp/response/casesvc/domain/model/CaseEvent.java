d4bfe2a64184d34dbda683a5bb29837697340cc1
Matt <matt.crocker@gmail.com>
2022-04-28 15:57:48 +0100

    Try removing the sequence from code instead

--- src/main/java/uk/gov/ons/ctp/response/casesvc/domain/model/CaseEvent.java
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/domain/model/CaseEvent.java
@@ -8,16 +8,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
@@ -29,21 +25,13 @@ import uk.gov.ons.ctp.response.casesvc.representation.CategoryDTO;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@TypeDefs({@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)})
+@TypeDefs({ @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class) })
 @Table(name = "caseevent", schema = "casesvc")
 public class CaseEvent implements Serializable {
 
   private static final long serialVersionUID = 6034836141646834386L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "caseeventseq_gen")
-  @GenericGenerator(
-      name = "caseeventseq_gen",
-      strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
-      parameters = {
-        @Parameter(name = "sequence_name", value = "casesvc.caseeventseq"),
-        @Parameter(name = "increment_size", value = "1")
-      })
   @Column(name = "case_event_pk")
   private Integer caseEventPK;
 
