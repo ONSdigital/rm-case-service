e1bccf15bcf059441520bd81892696ac6ce79600
Matt <matt.crocker@gmail.com>
2022-04-28 16:29:52 +0100

    If this works I'll be stunned

--- src/main/java/uk/gov/ons/ctp/response/casesvc/domain/model/CaseEvent.java
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/domain/model/CaseEvent.java
@@ -16,6 +16,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
@@ -34,9 +36,16 @@ public class CaseEvent implements Serializable {
   private static final long serialVersionUID = 6034836141646834386L;
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "caseeventseq_gen")
+  @GenericGenerator(
+      name = "caseeventseq_gen",
+      strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+      parameters = {
+        @Parameter(name = "sequence_name", value = "casesvc.caseeventseq"),
+        @Parameter(name = "increment_size", value = "1")
+      })
   @Column(name = "case_event_pk")
-  private Long caseEventPK;
+  private Integer caseEventPK;
 
   @Column(name = "case_fk")
   private Integer caseFK;
