72993ef334e946d96ece8f65b8d3414c3a5013d3
SteveScorfield <steve.scorfield@ons.gov.uk>
2022-08-17 15:00:52 +0100

    Fixed last failing test and reorganised ObjectConverter

--- src/main/java/uk/gov/ons/ctp/response/casesvc/domain/model/ObjectConverter.java
+++ src/main/java/uk/gov/ons/ctp/response/casesvc/domain/model/ObjectConverter.java
@@ -13,17 +13,49 @@ public class ObjectConverter {
 
   private ObjectConverter() {};
 
+  public static CaseDetailsDTO caseDetailsDTO(Case caze) {
+    CaseDetailsDTO caseDetailsDTO = new CaseDetailsDTO();
+
+    caseDetailsDTO.setActionPlanId(caze.getActionPlanId());
+    caseDetailsDTO.setActiveEnrolment(caze.isActiveEnrolment());
+    caseDetailsDTO.setCaseRef(caze.getCaseRef());
+    caseDetailsDTO.setCollectionInstrumentId(caze.getCollectionInstrumentId());
+    caseDetailsDTO.setCreatedBy(caze.getCreatedBy());
+    caseDetailsDTO.setCreatedDateTime(caze.getCreatedDateTime());
+    caseDetailsDTO.setIac(caze.getIac());
+    caseDetailsDTO.setId(caze.getId());
+    caseDetailsDTO.setPartyId(caze.getPartyId());
+    caseDetailsDTO.setSampleUnitId(caze.getSampleUnitId());
+    caseDetailsDTO.setSampleUnitType(caze.getSampleUnitType().name());
+    caseDetailsDTO.setState(caze.getState());
+
+    return caseDetailsDTO;
+  }
+
+  public static CaseEvent caseEvent(CaseEventCreationRequestDTO caseEventCreationRequestDTO) {
+    CaseEvent caseEvent = new CaseEvent();
+
+    caseEvent.setCategory(caseEventCreationRequestDTO.getCategory());
+    caseEvent.setCreatedBy(caseEventCreationRequestDTO.getCreatedBy());
+    caseEvent.setDescription(caseEventCreationRequestDTO.getDescription());
+    caseEvent.setMetadata(caseEventCreationRequestDTO.getMetadata());
+    caseEvent.setSubCategory(caseEventCreationRequestDTO.getSubCategory());
+
+    return caseEvent;
+  }
+
   public static List<CaseEventDTO> caseEventDTOList(List<CaseEvent> caseEvents) {
     List<CaseEventDTO> mappedCaseEventDTO = new ArrayList<>();
     for (int i = 1; i <= caseEvents.size(); i++) {
       CaseEvent caseEvent = caseEvents.get(i - 1);
       CaseEventDTO caseEventDTO = new CaseEventDTO();
+
       caseEventDTO.setCategory(caseEvent.getCategory());
       caseEventDTO.setCreatedBy(caseEvent.getCreatedBy());
+      caseEventDTO.setCreatedDateTime(caseEvent.getCreatedDateTime());
       caseEventDTO.setDescription(caseEvent.getDescription());
       caseEventDTO.setMetadata(caseEvent.getMetadata());
       caseEventDTO.setSubCategory(caseEvent.getSubCategory());
-      caseEventDTO.setCreatedDateTime(caseEvent.getCreatedDateTime());
 
       mappedCaseEventDTO.add(caseEventDTO);
     }
@@ -33,33 +65,14 @@ public class ObjectConverter {
   public static CreatedCaseEventDTO createdCaseEventDTO(CaseEvent caseEvent) {
     CreatedCaseEventDTO createdCaseEventDTO = new CreatedCaseEventDTO();
 
+    createdCaseEventDTO.setCategory(caseEvent.getCategory());
     createdCaseEventDTO.setCreatedBy(caseEvent.getCreatedBy());
     createdCaseEventDTO.setCreatedDateTime(caseEvent.getCreatedDateTime());
     createdCaseEventDTO.setDescription(caseEvent.getDescription());
-    createdCaseEventDTO.setCategory(caseEvent.getCategory());
     createdCaseEventDTO.setMetadata(caseEvent.getMetadata());
     createdCaseEventDTO.setSubCategory(caseEvent.getSubCategory());
 
     return createdCaseEventDTO;
-  }
-
-  public static CaseDetailsDTO caseDetailsDTO(Case caze) {
-    CaseDetailsDTO caseDetailsDTO = new CaseDetailsDTO();
-
-    caseDetailsDTO.setId(caze.getId());
-    caseDetailsDTO.setState(caze.getState());
-    caseDetailsDTO.setCaseRef(caze.getCaseRef());
-    caseDetailsDTO.setIac(caze.getIac());
-    caseDetailsDTO.setCreatedBy(caze.getCreatedBy());
-    caseDetailsDTO.setActionPlanId(caze.getActionPlanId());
-    caseDetailsDTO.setActiveEnrolment(caze.isActiveEnrolment());
-    caseDetailsDTO.setCreatedDateTime(caze.getCreatedDateTime());
-    caseDetailsDTO.setCollectionInstrumentId(caze.getCollectionInstrumentId());
-    caseDetailsDTO.setPartyId(caze.getPartyId());
-    caseDetailsDTO.setSampleUnitId(caze.getSampleUnitId());
-    caseDetailsDTO.setSampleUnitType(caze.getSampleUnitType().name());
-
-    return caseDetailsDTO;
   }
 
   public static CaseGroupDTO caseGroupDTO(CaseGroup caseGroup) {
@@ -77,29 +90,17 @@ public class ObjectConverter {
     return caseGroupDTO;
   }
 
-  public static CaseEvent caseEvent(CaseEventCreationRequestDTO caseEventCreationRequestDTO) {
-    CaseEvent caseEvent = new CaseEvent();
-
-    caseEvent.setDescription(caseEventCreationRequestDTO.getDescription());
-    caseEvent.setCategory(caseEventCreationRequestDTO.getCategory());
-    caseEvent.setCreatedBy(caseEventCreationRequestDTO.getCreatedBy());
-    caseEvent.setSubCategory(caseEventCreationRequestDTO.getSubCategory());
-    caseEvent.setMetadata(caseEventCreationRequestDTO.getMetadata());
-
-    return caseEvent;
-  }
-
   public static List<CategoryDTO> categoryDTO(List<Category> categories) {
     List<CategoryDTO> mappedCategoryDTO = new ArrayList<>();
     for (int i = 1; i <= categories.size(); i++) {
       Category category = categories.get(i - 1);
       CategoryDTO categoryDTO = new CategoryDTO();
 
-      categoryDTO.setShortDescription(category.getShortDescription());
       categoryDTO.setGroup(category.getGroup());
-      categoryDTO.setName(category.getCategoryName());
       categoryDTO.setLongDescription(category.getLongDescription());
+      categoryDTO.setName(category.getCategoryName());
       categoryDTO.setRole(category.getRole());
+      categoryDTO.setShortDescription(category.getShortDescription());
 
       mappedCategoryDTO.add(categoryDTO);
     }
