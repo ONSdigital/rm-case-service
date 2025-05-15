7fffda4a6455a8efcbcf00d30b917076c5d7f5ca
Mark Price <mark.price@ons.gov.uk>
2025-05-15 17:40:02 +0100

    Run wiremock in a container

--- wiremock-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-nohup java -jar /home/runner/.m2/repository/org/wiremock/wiremock-standalone/3.9.2/wiremock-standalone-3.9.2.jar --port 18002 > wiremock.log 2>&1 &
-echo $! > wiremock.pid
--- wiremock-stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -f wiremock.pid ]; then
-  if kill -0 $(cat wiremock.pid) 2>/dev/null; then
-    kill $(cat wiremock.pid) && rm wiremock.pid
-  else
-    rm wiremock.pid
-  fi
-fi
